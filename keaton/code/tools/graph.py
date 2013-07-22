c86aad07b70ff2d46d6c22a941abb0f57596fb5b
Keaton J. Burns <keaton.burns@gmail.com>
2013-07-22 14:52:39 -0700

    Create a simple directed graph class. Vertices and edges can be arbitrary objects.  Arbitrary vertex properties can be used as path search criteria.  Weighting based on arbitrary edge properties can easily be added.
    hg hash: 1b0cf4b8153b813487718cd9b9ef07e6ee51894b

---
+++ keaton/code/tools/graph.py
@@ -0,0 +1,120 @@
+
+
+import numpy as np
+
+
+class Graph(object):
+    """Basic directed graph."""
+
+    def __init__(self):
+
+        # Store edges as dictionary: {start: {end: edge, ...}, ...}
+        self.graph = {}
+
+    def add_vertex(self, vertex):
+
+        # Create empty edge dictionary
+        if vertex not in self.graph:
+            self.graph[vertex] = {}
+
+    def add_edge(self, start, end, edge):
+
+        # Add start and end vertices
+        # Note: Adding the end vertex is required, otherwise the search methods
+        # will fail if there are any dead-end vertices.
+        self.add_vertex(start)
+        self.add_vertex(end)
+
+        # Add edge
+        self.graph[start][end] = edge
+
+    def find_vertex_paths(self, start, test, path=[]):
+
+        # Add start to path (keyword used in recursion)
+        path = path + [start]
+
+        # Check test
+        if test(start):
+            # Pass: done
+            paths = [path]
+        else:
+            # Fail: iterate over connected nodes
+            paths = []
+            for node in self.graph[start]:
+                # Only examine unvisited nodes
+                if node not in path:
+                    # Recurse
+                    subpaths = self.find_vertex_paths(node, test, path)
+                    for sp in subpaths:
+                        paths.append(sp)
+
+        return paths
+
+    def find_edge_paths(self, start, test):
+
+        # Get vertex paths
+        vertex_paths = self.find_vertex_paths(start, test)
+
+        # Iterate over vertex paths
+        paths = []
+        for vp in vertex_paths:
+            # Loop through vertices
+            path = []
+            n_edges = len(vp) - 1
+            for i in xrange(n_edges):
+                # Get edge from graph
+                current = vp[i]
+                next = vp[i+1]
+                path.append(self.graph[current][next])
+            paths.append(path)
+
+        return paths
+
+    def find_shortest_path(self, start, test):
+
+        # Get edge paths
+        paths = self.find_edge_paths(start, test)
+
+        # Check if any paths exist
+        # Note: this is to distinguish between no possible paths (paths = [])
+        # and the trivial/empty path (paths = [[]]), i.e. where start satisfies test.
+        if len(paths) == 0:
+            raise ValueError("No paths found.")
+
+        # Count edges
+        steps = [len(p) for p in paths]
+
+        # Return shortest
+        shortest = np.argmin(steps)
+
+        return paths[shortest]
+
+
+def local(i):
+    test = lambda layout: layout.local[i]
+    return test
+
+
+class Layout(object):
+    pass
+
+A = Layout()
+A.local = [False, False, True]
+
+B = Layout()
+B.local = [False, True, False]
+
+C = Layout()
+C.local = [False, False, False]
+
+
+graph = Graph()
+graph.add_vertex(A)
+graph.add_vertex(B)
+graph.add_vertex(C)
+
+graph.add_edge(A, B, 'A2B')
+graph.add_edge(A, C, 'A2C')
+graph.add_edge(B, A, 'B2A')
+graph.add_edge(C, A, 'C2A')
+graph.add_edge(C, B, 'C2B')
---
+++ keaton/code/tools/graphtest.py
@@ -0,0 +1,39 @@
+
+
+from graph import Graph
+
+
+class Layout(object):
+    pass
+
+A = Layout()
+A.local = [False, False, True]
+
+B = Layout()
+B.local = [False, True, False]
+
+C = Layout()
+C.local = [True, False, False]
+
+
+def local(i):
+    test = lambda layout: layout.local[i]
+    return test
+
+
+graph = Graph()
+graph.add_vertex(A)
+graph.add_vertex(B)
+graph.add_vertex(C)
+
+graph.add_edge(A, B, 'A2B')
+graph.add_edge(B, A, 'B2A')
+
+graph.add_edge(B, C, 'B2C')
+graph.add_edge(C, B, 'C2B')
+
+graph.add_edge(A, C, 'A2C')
+
+
+print graph.find_edge_paths(A, local(0))
+
