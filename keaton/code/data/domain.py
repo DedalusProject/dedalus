d218c07bea32e029218ddb0dc1f6bfeda086f638
Keaton J. Burns <keaton.burns@gmail.com>
2013-07-26 10:10:34 -0700

    Change domain grid structure
    hg hash: dd23fa3d3690183400efa7ed7803d30062267fe3

--- keaton/code/data/domain.py
+++ keaton/code/data/domain.py
@@ -16,8 +16,11 @@ class Domain(object):
         self.dim = len(self.shape)
 
         # Grid
-        self.grid = bases[-1].grid
+        self.grids = [b.grid for b in bases]
+        if self.dim > 1:
+            self.grids = np.meshgrid(*self.grids, indexing='ij')
 
         # Pencil slices
+        num_pencils = np.prod(self.shape) / self.shape[-1]
         self.slices = [slice(None)]
 
