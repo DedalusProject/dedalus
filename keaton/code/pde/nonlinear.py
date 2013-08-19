46d50d75a4082107243b7ac18c79bf813705c69c
Keaton J. Burns <keaton.burns@gmail.com>
2013-08-19 12:08:01 -0700

    Clarify references
    hg hash: 3b6bcca1c4230d0d474864bd392c6f7ced208a27

--- keaton/code/pde/nonlinear.py
+++ keaton/code/pde/nonlinear.py
@@ -3,10 +3,10 @@
 from scipy import sparse
 
 
-def get_fields(rhs_expressions):
+def get_fields(expressions):
 
     fields = set()
-    for re in rhs_expressions:
+    for re in expressions:
         if re is not None:
             fields.update(re.field_set())
 
@@ -16,16 +16,16 @@ def get_fields(rhs_expressions):
 def compute_expressions(rhs_expressions, out_system):
 
     # Make local copy of list
-    rhs_expressions = [re for re in rhs_expressions]
+    expressions = list(rhs_expressions)
 
     # Skip where no F is specified
-    for i, re in enumerate(rhs_expressions):
+    for i, re in enumerate(expressions):
         if re is None:
             fn = out_system.field_names[i]
             out_system[fn]['K'] = 0.
 
     # Start all from k space
-    fields = get_fields(rhs_expressions)
+    fields = get_fields(expressions)
     for f in fields:
         f.require_global_space('K')
 
@@ -35,34 +35,34 @@ def compute_expressions(rhs_expressions, out_system):
     for i in reversed(range(dimensions)):
 
         # Transpose fields
-        fields = get_fields(rhs_expressions)
+        fields = get_fields(expressions)
         for f in fields:
             f.require_local(i)
 
         # Attempt evaluation
-        for j, re in enumerate(rhs_expressions):
+        for j, re in enumerate(expressions):
             if re is not None:
                 re_eval = re.evaluate()
                 if re_eval is not None:
                     fn = out_system.field_names[j]
                     space = ''.join(re_eval.space)
                     out_system[fn][space] = re_eval[space]
-                    rhs_expressions[j] = None
+                    expressions[j] = None
 
         # Transform fields
-        fields = get_fields(rhs_expressions)
+        fields = get_fields(expressions)
         for f in fields:
             f.require_space(i, 'x')
 
         # Attempt evaluation
-        for j, re in enumerate(rhs_expressions):
+        for j, re in enumerate(expressions):
             if re is not None:
                 re_eval = re.evaluate()
                 if re_eval is not None:
                     fn = out_system.field_names[j]
                     space = ''.join(re_eval.space)
                     out_system[fn][space] = re_eval[space]
-                    rhs_expressions[j] = None
+                    expressions[j] = None
 
     # Non-linear products should now be computed (we are in X)
 
@@ -72,34 +72,34 @@ def compute_expressions(rhs_expressions, out_system):
     for i in range(dimensions):
 
         # Transpose fields
-        fields = get_fields(rhs_expressions)
+        fields = get_fields(expressions)
         for f in fields:
             f.require_local(i)
 
         # Attempt evaluation
-        for j, re in enumerate(rhs_expressions):
+        for j, re in enumerate(expressions):
             if re is not None:
                 re_eval = re.evaluate()
                 if re_eval is not None:
                     fn = out_system.field_names[j]
                     space = ''.join(re_eval.space)
                     out_system[fn][space] = re_eval[space]
-                    rhs_expressions[j] = None
+                    expressions[j] = None
 
         # Transform fields
-        fields = get_fields(rhs_expressions)
+        fields = get_fields(expressions)
         for f in fields:
             f.require_space(i, 'k')
 
         # Attempt evaluation
-        for j, re in enumerate(rhs_expressions):
+        for j, re in enumerate(expressions):
             if re is not None:
                 re_eval = re.evaluate()
                 if re_eval is not None:
                     fn = out_system.field_names[j]
                     space = ''.join(re_eval.space)
                     out_system[fn][space] = re_eval[space]
-                    rhs_expressions[j] = None
+                    expressions[j] = None
 
     # If necessary, perform this multiple times until everything is computed
     #   (In case someone does "dx(u*v) * dx(u*v)" or similar)
