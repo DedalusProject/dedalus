c6afe8525a4aadd7c2f4a60eb1868abd12200ab2
Keaton J. Burns <keaton.burns@gmail.com>
2013-09-16 23:56:41 -0400

    Update tau matrix names in pencil class
    hg hash: 09398d1dd3c7e6df206a203adb2775d0e3b658de

--- dedalus2/data/pencil.py
+++ dedalus2/data/pencil.py
@@ -27,20 +27,20 @@ class Pencil:
         # Set slice of all fields
         start = 0
         for field in system.fields.values():
-            end = start + field.domain.bases[-1].size
+            end = start + field.domain.bases[-1].coeff_size
             field['k'][self.slice] = data[start:end]
             start = end
 
     def build_matrices(self, problem, basis):
 
         # Instruct basis to build its matrices
-        basis.build_matrices()
+        basis.build_tau_matrices(problem.order)
 
         # Build PDE matrices starting with constant terms
-        M = (sparse.kron(problem.M0[0], basis.Eval) +
-             sparse.kron(problem.M1[0], basis.Deriv))
-        L = (sparse.kron(problem.L0[0], basis.Eval) +
-             sparse.kron(problem.L1[0], basis.Deriv))
+        M = (sparse.kron(problem.M0[0], basis.Pre) +
+             sparse.kron(problem.M1[0], basis.Pre * basis.Diff))
+        L = (sparse.kron(problem.L0[0], basis.Pre) +
+             sparse.kron(problem.L1[0], basis.Pre * basis.Diff))
 
         # Convert to easily modifiable structures
         M = M.tolil()
@@ -48,14 +48,13 @@ class Pencil:
 
         # Add higher order terms
         for i in range(1, problem.order):
-            Mult_i = basis._build_Mult(i)
-            Eval_i = basis.Eval * Mult_i
-            Deriv_i = basis.Eval * Mult_i * basis.InvEval * basis.Deriv
+            Pre_i = basis.Pre * basis.Mult[i]
+            Diff_i = basis.Pre * basis.Mult[i] * basis.Diff
 
-            M += sparse.kron(problem.M0[i], Eval_i)
-            M += sparse.kron(problem.M1[i], Deriv_i)
-            L += sparse.kron(problem.L0[i], Eval_i)
-            L += sparse.kron(problem.L1[i], Deriv_i)
+            M += sparse.kron(problem.M0[i], Pre_i)
+            M += sparse.kron(problem.M1[i], Diff_i)
+            L += sparse.kron(problem.L0[i], Pre_i)
+            L += sparse.kron(problem.L1[i], Diff_i)
 
         # Build boundary condition matrices
         Mb = (sparse.kron(problem.ML, basis.Left) +
@@ -89,5 +88,5 @@ class Pencil:
         self.bc_rows = list(rows)
         self.bc_f = [self.b[r] for r in rows]
         self.parameters = problem.parameters
-        self.F_eval = sparse.kron(np.eye(problem.size), basis.Eval)
+        self.F_eval = sparse.kron(np.eye(problem.size), basis.Pre)
 
