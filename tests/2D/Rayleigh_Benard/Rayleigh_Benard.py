35fdd5a58c712170eefda50ec3b0da76913d53b1
Keaton J. Burns <keaton.burns@gmail.com>
2013-09-20 11:25:14 -0400

    Modify RB test
    hg hash: 0f9ca38e6746acaef40eceb95c4fd6136b72e7e7

--- tests/2D/Rayleigh_Benard/Rayleigh_Benard.py
+++ tests/2D/Rayleigh_Benard/Rayleigh_Benard.py
@@ -1,20 +1,31 @@
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import shelve
 from dedalus2.public import *
 
-# set domain
-x_basis = Fourier(16, interval=[0.,2.*np.pi])
-z_basis = Chebyshev(16, interval=[0.,1.])
-domain = Domain([x_basis,z_basis])
 
-Ra = 800.
+# Set domain
+x_basis = Fourier(32, interval=[0.,1.])
+z_basis = Chebyshev(32, interval=[0.,1.])
+domain = Domain([x_basis, z_basis])
+
+Ra = 1000.
 Pr = 1.
 iPr = 1./Pr
 
 rb = Problem(['p','u','w','t','uz','tz'], 1)
+# Equations:
+#
+# Dx u + Dz w = 0
+# iPr*Dt u + Dx p - Dx^2 u - Dz uz = - iPr * ( u Dx u + w uz )
+# iPr*Dt w + Dz p - Ra t - Dx^2 w + Dx uz = - iPr * (u Dx w - w Dx u)
+# Dt t - w - Dx^2 t - Dz tz = - u Dx t - w tz
+# Dz u - uz = 0
+# Dz t - tz = 0
+#
 
 def M(d_trans):
   return np.array([[0,   0,   0,   0, 0, 0],
@@ -60,7 +71,7 @@ rb.LL = lambda d_trans: np.array([[ 0, 0, 1.0,   0,   0, 0],
                                   [ 0, 0,   0,   0,   0, 0]])
 
 rb.LR = lambda d_trans: np.array([[ 0, 0,   0,   0,   0, 0],
-                                  [ 0, 0,   0,   0,   0, 0],                                     
+                                  [ 0, 0,   0,   0,   0, 0],
                                   [ 0, 0,   0,   0,   0, 0],
                                   [ 0, 0, 1.0,   0,   0, 0],
                                   [ 0, 0,   0, 1.0,   0, 0],
@@ -79,21 +90,22 @@ z = domain.grids[1]
 u  = int.state['u']
 uz = int.state['uz']
 w  = int.state['w']
-u['X']   = np.cos(np.pi * 2. * z) * np.cos(x) * np.pi*2.
+T = int.state['t']
+u['X']   = np.cos(np.pi * 2. * z) * np.cos(2*np.pi*x + np.pi/4.)
 uz['xk'] = u.differentiate(1)
-w['X']   = np.sin(np.pi * 2. * z) * np.sin(x)
+w['X']   = np.sin(np.pi * 2. * z) * np.sin(2*np.pi*x + np.pi/4.)
 
-wz = field_manager.get_field(domain)
-ux = field_manager.get_field(domain)
+# wz = field_manager.get_field(domain)
+# ux = field_manager.get_field(domain)
 
-wz['xk'] = w.differentiate(1)
-ux['K'] = u.differentiate(0)
+# wz['xk'] = w.differentiate(1)
+# ux['K'] = u.differentiate(0)
 
 #psi = field_manager.get_field(domain)
 
 # integrate parameters
 
-int.dt = 1e-2
+int.dt = 1e-3
 int.sim_stop_time = 1.0
 int.wall_stop_time = np.inf
 int.stop_iteration = np.inf
@@ -101,6 +113,8 @@ int.stop_iteration = np.inf
 # storage lists
 t_list = [int.time]
 u_list = [np.copy(u['X'])]
+w_list = [np.copy(w['X'])]
+T_list = [np.copy(T['X'])]
 copy_cadence = 10
 
 # Main loop
@@ -114,11 +128,14 @@ while int.ok:
   if int.iteration % copy_cadence == 0:
     t_list.append(int.time)
     u_list.append(np.copy(u['X']))
+    w_list.append(np.copy(w['X']))
+    T_list.append(np.copy(T['X']))
     print('Iteration: %i, Time: %e' %(int.iteration, int.time))
 
-if int.iteration % copy_candence != 0:
+if int.iteration % copy_cadence != 0:
   t_list.append(int.time)
   u_list.append(np.copy(u['X']))
+  T_list.append(np.copy(T['X']))
 
 end_time = time.time()
 
@@ -133,7 +150,10 @@ print('-' * 20)
 shelf = shelve.open('data.db', flag='n')
 shelf['t'] = np.array(t_list)
 shelf['x'] = x
-shelf['y'] = np.array(y_list)
+shelf['z'] = z
+shelf['u'] = np.array(u_list)
+shelf['w'] = np.array(w_list)
+shelf['T'] = np.array(T_list)
 shelf.close()
 
 
