f05c2e83a85f6f1fab3c405e434c6d1368c8a931
Daniel Lecoanet <dlecoanet@gmail.com>
2013-09-23 00:03:19 -0400

    Added in a new test problem (the simplified convection problem), and a pdf with the derivation of the various equations.
    hg hash: 6f4673f6d21330d14c4bc2fda9f9fc4aa1752e6d

---
+++ tests/2D/conv_model/conv_model.py
@@ -0,0 +1,129 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import shelve
+from dedalus2.public import *
+
+# set domain
+x_basis = Fourier(32, interval=[0.,1.])
+z_basis = Chebyshev(32, interval=[0.,1.])
+domain = Domain([x_basis,z_basis])
+
+Ra = 2000.
+
+conv = Problem(['w','t','wz','tz'], 1)
+
+# Equations:
+#
+# Dt w - Ra t - Dx^2 w + Dz wz = 0
+# Dt t - w - Dx^2 t - Dz tz = 0
+# Dz w - wz = 0
+# Dz t - tz = 0
+# 
+
+
+def M(d_trans):
+  return np.array([[1.0,   0,   0,   0],
+                   [  0, 1.0,   0,   0],
+                   [  0,   0,   0,   0],
+                   [  0,   0,   0,   0]])
+
+conv.M0[0] = M
+
+def L0(d_trans):
+  Dx = d_trans[0]
+  return np.array([[ -Dx**2,    -Ra,     0,   0],
+                   [   -1.0, -Dx**2,     0,   0],
+                   [      0,      0, -1.0,    0],
+                   [      0,      0,    0, -1.0]])
+
+
+conv.L0[0] = L0
+
+def L1(d_trans):
+  return np.array([[   0,   0, -1.0,    0],
+                   [   0,   0,    0, -1.0],
+                   [ 1.0,   0,    0,    0],
+                   [   0, 1.0,    0,    0]])
+
+conv.L1[0] = L1
+
+conv.LL = lambda d_trans: np.array([[ 1.0,   0, 0, 0],
+                                  [   0, 1.0, 0, 0],
+                                  [   0,   0, 0, 0],
+                                  [   0,   0, 0, 0]])
+
+conv.LR = lambda d_trans: np.array([[   0,   0, 0, 0],
+                                  [   0,   0, 0, 0],
+                                  [ 1.0,   0, 0, 0],
+                                  [   0, 1.0, 0, 0]])
+
+
+pde = conv
+ts = timesteppers.CNAB3
+
+# Build solver
+int = Integrator(pde, domain, ts)
+
+# initial conditions
+x = domain.grids[0]
+z = domain.grids[1]
+w  = int.state['w']
+wz = int.state['wz']
+t = int.state['t']
+w['X']   = np.sin(np.pi * 2. * z) * np.sin(np.pi * 2. * x)
+wz['xk'] = w.differentiate(1)
+
+# integrate parameters
+
+int.dt = 2e-2
+int.sim_stop_time = 1.0
+int.wall_stop_time = np.inf
+int.stop_iteration = np.inf
+
+# storage lists
+t_list = [int.time]
+w_list = [np.copy(w['X'])]
+theta_list = [np.copy(t['X'])]
+copy_cadence = 1
+
+# Main loop
+start_time = time.time()
+while int.ok:
+
+  # advance
+  int.advance()
+
+  # update lists
+  if int.iteration % copy_cadence == 0:
+    t_list.append(int.time)
+    w_list.append(np.copy(w['X']))
+    theta_list.append(np.copy(t['X']))
+    print('Iteration: %i, Time: %e' %(int.iteration, int.time))
+
+if int.iteration % copy_cadence != 0:
+  t_list.append(int.time)
+  w_list.append(np.copy(w['X']))
+
+end_time = time.time()
+
+# Print statistics
+print('-' * 20)
+print('Total time:', end_time - start_time)
+print('Iterations:', int.iteration)
+print('Average timestep:', int.time / int.iteration)
+print('-' * 20)
+
+# Write storage lists
+shelf = shelve.open('data.db', flag='n')
+shelf['t'] = np.array(t_list)
+shelf['x'] = x
+shelf['z'] = z
+shelf['w'] = np.array(w_list)
+shelf.close()
+
+
+
+
+
