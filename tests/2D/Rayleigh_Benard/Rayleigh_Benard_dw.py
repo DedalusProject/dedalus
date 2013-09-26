89dc314987f394b4e3a8cd23ba25b12878640a6a
Daniel Lecoanet <dlecoanet@gmail.com>
2013-09-26 10:22:01 -0700

    New version of the RB problem which uses dw as an explicit variable.  This new formulation gives the same results as the old formulation, but is a bit slower.
    hg hash: 810345aab17600351c38c12bbd62b303ee4e450f

--- tests/2D/Rayleigh_Benard/Rayleigh_Benard_dw.py
+++ tests/2D/Rayleigh_Benard/Rayleigh_Benard_dw.py
@@ -8,54 +8,58 @@ from dedalus2.public import *
 
 
 # Set domain
-x_basis = Fourier(32, interval=[0., 1])
-z_basis = Chebyshev(32, interval=[0.,1.])
+x_basis = Fourier(16, interval=[0.,1.])
+z_basis = Chebyshev(16, interval=[0.,1.])
 domain = Domain([x_basis, z_basis])
 
 Ra = 3000.
 Pr = 1.
 iPr = 1./Pr
 
-rb = Problem(['p','u','w','t','uz','tz'], 1)
+rb = Problem(['p','u','w','t','uz','wz','tz'], 1)
 # Equations:
 #
-# Dx u + Dz w = 0
+# Dx u + wz = 0
 # iPr Dt u + Dx p        - Dx Dx u - Dz uz = - iPr (u Dx u + w uz  )
-# iPr Dt w + Dz p - Ra t - Dx Dx w + Dx uz = - iPr (u Dx w - w Dx u)
+# iPr Dt w + Dz p - Ra t - Dx Dx w - Dz wz = - iPr (u Dx w - w Dx u)
 # Dt t - w - Dx Dx t - Dz tz = - u Dx t - w tz
 # Dz u - uz = 0
+# Dz w - wz = 0
 # Dz t - tz = 0
 #
 
 def M(d_trans):
-  return np.array([[0,   0,   0,   0, 0, 0],
-                   [0, iPr,   0,   0, 0, 0],
-                   [0,   0, iPr,   0, 0, 0],
-                   [0,   0,   0, 1.0, 0, 0],
-                   [0,   0,   0,   0, 0, 0],
-                   [0,   0,   0,   0, 0, 0]])
+  return np.array([[0,   0,   0,   0, 0, 0, 0],
+                   [0, iPr,   0,   0, 0, 0, 0],
+                   [0,   0, iPr,   0, 0, 0, 0],
+                   [0,   0,   0, 1.0, 0, 0, 0],
+                   [0,   0,   0,   0, 0, 0, 0],
+                   [0,   0,   0,   0, 0, 0, 0],
+                   [0,   0,   0,   0, 0, 0, 0]])
 
 rb.M0[0] = M
 
 def L0(d_trans):
   Dx = d_trans[0]
-  return np.array([[ 0,     Dx,      0,      0,    0,    0],
-                   [Dx, -Dx*Dx,      0,      0,    0,    0],
-                   [ 0,      0, -Dx*Dx,    -Ra,   Dx,    0],
-                   [ 0,      0,   -1.0, -Dx*Dx,    0,    0],
-                   [ 0,      0,      0,      0, -1.0,    0],
-                   [ 0,      0,      0,      0,    0, -1.0]])
+  return np.array([[ 0,     Dx,      0,      0,   0,  1.,   0],
+                   [Dx, -Dx*Dx,      0,      0,   0,   0,   0],
+                   [ 0,      0, -Dx*Dx,    -Ra,   0,   0,   0],
+                   [ 0,      0,    -1., -Dx*Dx,   0,   0,   0],
+                   [ 0,      0,      0,      0, -1.,   0,   0],
+                   [ 0,      0,      0,      0,   0, -1.,   0],
+                   [ 0,      0,      0,      0,   0,   0, -1.]])
 
 
 rb.L0[0] = L0
 
 def L1(d_trans):
-  return np.array([[   0,   0, 1.0,   0,    0,    0],
-                   [   0,   0,   0,   0, -1.0,    0],
-                   [ 1.0,   0,   0,   0,    0,    0],
-                   [   0,   0,   0,   0,    0, -1.0],
-                   [   0, 1.0,   0,   0,    0,    0],
-                   [   0,   0,   0, 1.0,    0,    0]])
+  return np.array([[  0,  0,  0,  0,   0,   0,   0],
+                   [  0,  0,  0,  0, -1.,   0,   0],
+                   [ 1.,  0,  0,  0,   0, -1.,   0],
+                   [  0,  0,  0,  0,   0,   0, -1.],
+                   [  0, 1.,  0,  0,   0,   0,   0],
+                   [  0,  0, 1.,  0,   0,   0,   0],
+                   [  0,  0,  0, 1.,   0,   0,   0]])
 
 rb.L1[0] = L1
 
@@ -63,19 +67,21 @@ rb.L1[0] = L1
 # rb.F[2] = "-1/Pr*(u*d_trans[0]*w-w*d_trans[0]*u)"
 # rb.F[3] = "-u*d_trans[0]*t-w*tz"
 
-rb.LL = lambda d_trans: np.array([[ 0, 0, 1.0,   0,   0, 0],
-                                  [ 0, 0,   0, 1.0,   0, 0],
-                                  [ 0, 0,   0,   0, 1.0, 0],
-                                  [ 0, 0,   0,   0,   0, 0],
-                                  [ 0, 0,   0,   0,   0, 0],
-                                  [ 0, 0,   0,   0,   0, 0]])
+rb.LL = lambda d_trans: np.array([[ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0, 1.0,   0,   0, 0, 0],
+                                  [ 0, 0,   0, 1.0,   0, 0, 0],
+                                  [ 0, 0,   0,   0, 1.0, 0, 0],
+                                  [ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0,   0,   0,   0, 0, 0]])
 
-rb.LR = lambda d_trans: np.array([[ 0, 0,   0,   0,   0, 0],
-                                  [ 0, 0,   0,   0,   0, 0],
-                                  [ 0, 0,   0,   0,   0, 0],
-                                  [ 0, 0, 1.0,   0,   0, 0],
-                                  [ 0, 0,   0, 1.0,   0, 0],
-                                  [ 0, 0,   0,   0, 1.0, 0]])
+rb.LR = lambda d_trans: np.array([[ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0,   0,   0,   0, 0, 0],
+                                  [ 0, 0, 1.0,   0,   0, 0, 0],
+                                  [ 0, 0,   0, 1.0,   0, 0, 0],
+                                  [ 0, 0,   0,   0, 1.0, 0, 0]])
 
 
 pde = rb
@@ -90,6 +96,7 @@ z = domain.grids[1]
 u  = int.state['u']
 uz = int.state['uz']
 w  = int.state['w']
+wz = int.state['wz']
 T = int.state['t']
 Tz = int.state['tz']
 #T['X'] = 1e-10 * np.sin(np.pi * z) * np.random.randn(*T['X'].shape) + np.sin(np.pi*2.*z)
@@ -97,6 +104,7 @@ Tz = int.state['tz']
 u['X']   = np.cos(1.*np.pi * z) * np.cos(2.*np.pi*x + np.pi/4.)
 uz['xk'] = u.differentiate(1)
 w['X']   = 2.*np.sin(1.*np.pi * z) * np.sin(2.*np.pi*x + np.pi/4.)
+wz['xk'] = w.differentiate(1)
 
 # wz = field_manager.get_field(domain)
 # ux = field_manager.get_field(domain)
