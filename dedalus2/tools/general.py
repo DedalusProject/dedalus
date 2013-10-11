2beaff1e882d6ea1483640c360bef8654ea3c1a1
Keaton J. Burns <keaton.burns@gmail.com>
2013-10-11 11:09:15 -0400

    Upgrade CachedMethod descriptor, fixing memory issue associated with previous patch.
    hg hash: dc9427d699f5a374233843fbc10fb88234f1f927

--- dedalus2/tools/general.py
+++ dedalus2/tools/general.py
@@ -1,6 +1,6 @@
 
 
-import functools
+import types
 
 
 class CachedAttribute:
@@ -10,6 +10,7 @@ class CachedAttribute:
 
         # Parameters
         self.method = method
+        self.__name__ = method.__name__
         self.__doc__ = method.__doc__
 
     def __get__(self, instance, owner):
@@ -21,8 +22,8 @@ class CachedAttribute:
         # Build attribute
         attribute = self.method(instance)
 
-        # Cache attribute (overwriting descriptor)
-        setattr(instance, self.method.__name__, attribute)
+        # Set as instance attribute
+        setattr(instance, self.__name__, attribute)
 
         return attribute
 
@@ -34,19 +35,9 @@ class CachedMethod:
 
         # Parameters
         self.method = method
+        self.__name__ = method.__name__
         self.__doc__ = method.__doc__
         self.cache = dict()
-
-    def __call__(self, instance, *args):
-
-        if instance not in self.cache:
-            self.cache[instance] = dict()
-
-        # Call method for new arguments
-        if args not in self.cache[instance]:
-            self.cache[instance][args] = self.method(instance, *args)
-
-        return self.cache[instance][args]
 
     def __get__(self, instance, owner):
 
@@ -54,6 +45,40 @@ class CachedMethod:
         if instance is None:
             return self
 
-        # Return partial call (with access to 'instance')
-        return functools.partial(self.__call__, instance)
+        # Build new cached method and bind to instance
+        new_cached_method = CachedMethod(self.method)
+        bound_method = types.MethodType(new_cached_method, instance)
+
+        # Set as instance method
+        setattr(instance, self.__name__, bound_method)
+
+        return bound_method
+
+    def __call__(self, instance, *args):
+
+        # Call method for new arguments
+        if args not in self.cache:
+            self.cache[args] = self.method(instance, *args)
+
+        return self.cache[args]
+
+
+class CachedFunction:
+    """Decorator for caching function outputs during first call."""
+
+    def __init__(self, function):
+
+        # Parameters
+        self.function = function
+        self.__name__ = function.__name__
+        self.__doc__ = function.__doc__
+        self.cache = dict()
+
+    def __call__(self, *args):
+
+        # Call function for new arguments
+        if args not in self.cache:
+            self.cache[args] = self.function(*args)
+
+        return self.cache[args]
 
