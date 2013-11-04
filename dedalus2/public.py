b87209b2d949d125ddec07c1036f870b89f44664
Keaton J. Burns <keaton.burns@gmail.com>
2013-11-04 13:28:50 -0500

    Remove field manager from public interface
    hg hash: 9500aa44f82a4cb3a9628e321de501f41f5c2f1e

--- dedalus2/public.py
+++ dedalus2/public.py
@@ -1,7 +1,6 @@
 
 
 from .data.domain import Domain
-from .data.field import field_manager
 from .data import operators
 from .pde.basis import Chebyshev, Fourier
 from .pde.integrator import Integrator
