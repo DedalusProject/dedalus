26d1b9ef477de01f7bec006ed04167d3a46e3c15
Keaton J. Burns <keaton.burns@gmail.com>
2013-11-04 13:03:53 -0500

    Add interleaved_view and reshape_vector to tools
    hg hash: 66133a1e9dec597274acbeb0bfb8e503686b249c

--- dedalus2/tools/general.py
+++ dedalus2/tools/general.py
@@ -82,3 +82,33 @@ class CachedFunction:
 
         return self.cache[args]
 
+
+def interleaved_view(data):
+    """
+    View n-dim complex array as (n+1)-dim real array, where the last axis
+    separates real and imaginary parts.
+
+    """
+
+    # Check datatype
+    if data.dtype is not np.complex128:
+        raise ValueError("Complex array required.")
+
+    # Build new shape
+    shape = list(data.shape).append(2)
+
+    # Create view array
+    view = np.ndarray(shape, dtype=np.float64, buffer=data.data)
+
+    return view
+
+
+def reshape_vector(data, dim=2, axis=-1):
+    """Reshape 1-dim array as a multidimensional vector."""
+
+    # Build multidimensional shape
+    shape = [1] * dim
+    shape[axis] = data.size
+
+    return data.reshape(shape)
+
