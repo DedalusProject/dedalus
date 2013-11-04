54e38383baba8a9eccc5cf5fe747bee3cc919ad9
Keaton J. Burns <keaton.burns@gmail.com>
2013-11-04 13:04:56 -0500

    Rework domain to incorporate distributor, and improve field management
    hg hash: e6f4a7af16d39013e2ef16f5fc730b187dbff670

--- dedalus2/data/domain.py
+++ dedalus2/data/domain.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 from .field import Field
+from ..tools.general import CachedMethod, reshape_vector
 
 
 class Domain:
@@ -18,16 +19,23 @@ class Domain:
         # Iteratively set basis data types
         # (Transforms from grid-to-coeff proceed in the listed order)
         for b in self.bases:
-            grid_dtype = b.set_dtype(grid_dtype)
-
-        # Construct domain grids
-        grids = [b.grid for b in bases]
-        if self.dim > 1:
-            self.grids = np.meshgrid(*grids, indexing='ij')
+            grid_dtype = b.set_dtypes(grid_dtype)
 
         # Construct pencil slices
-        n_pencils = np.prod([b.coeff_size for b in bases])
-        n_pencils /= bases[-1].coeff_size
+        self._construct_pencils()
+
+        # Field management
+        self._field_list = list()
+        self._field_count = 0
+
+        # Create distributor
+        self.distributor = Distributor(self)
+
+    def _construct_pencils(self):
+
+        # Construct pencil slices
+        n_pencils = np.prod([b.coeff_size for b in self.bases])
+        n_pencils /= self.bases[-1].coeff_size
         n = np.arange(int(n_pencils))
         index_list = []
         dtrans_list = []
@@ -47,54 +55,42 @@ class Domain:
                     index_list = list(zip(*index_list))
                     dtrans_list = list(zip(*dtrans_list))
 
-        self.dtrans_list = dtrans_list
-        self.slices = []
         for bl in index_list:
             sli = []
             for i in bl:
                 sli.append(slice(i, i+1))
             sli.append(slice(None))
-            self.slices.append(sli)
+            slices.append(sli)
 
-        # Field management
-        self._field_list = list()
-        self._field_count = 0
+        # Build pencil objects
+        self.pencils = []
+        for s, d in zip(slices, dtrans_list):
+            self.pencils.append(Pencil(s, d))
 
-    # def grid(self, layout):
+    @CachedMethod
+    def grid(self, axis):
 
-    #     slices = layout.slices
-    #     grids = self.grids
+        sli = self.distributor.grid_layout.slices[axis]
+        grid = self.bases[axis].grid[sli]
+        grid = reshape_vector(grid, self.dim, axis)
 
-    #     local_grids = [g[s] for (g, s) in zip(grids, slices)]
-
-    #     return np.meshgrid(*local_grids, indexing='ij')
-
-    def integrate(self, field):
-        """Integrate field over domain."""
-
-        # Integrate by coefficients
-        data = field['K']
-        for b in self.bases:
-            data = b.integrate(data, 0)
-            data = b.grid_dtype(data)
-
-        return data
+        return grid
 
     def _collect_field(self, field):
 
         # Clean field
-        # field.layout = distributor.layouts[field.domain][0]
-        # field.data *= 0.
+        field.layout = self.distributor.grid_layout
+        field.data.fill(0)
 
         # Add to field list
         self._field_list.append(field)
 
-    def get_field(self):
+    def new_field(self):
 
         # Return a free field if available
         if self._field_list:
             field = self._field_list.pop()
-        # Otherwise build a new field
+        # Otherwise instantiate another field
         else:
             field = Field(self)
 
