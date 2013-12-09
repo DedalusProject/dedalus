a0abc1f9f70e2460cd24c7d9e4a9a98f04137cf2
Keaton J. Burns <keaton.burns@gmail.com>
2013-12-09 13:24:22 -0500

    Add multiple-dispatch metaclass that directs instantiation based on args
    hg hash: 257efe3ea7a0a5f48931bb5d5457b7c201d7e11e

---
+++ dedalus2/tools/dispatch.py
@@ -0,0 +1,28 @@
+
+
+class MultiClass(type):
+    """Metaclass that dispatches instantiation to subclasses."""
+
+    def __call__(cls, *args, **kw):
+        """Dispatch instantiation based on the supplied arguments."""
+
+        # Dispatch if subclasses exist
+        if cls.__subclasses__():
+
+            # Find applicable subclasses
+            passlist = []
+            for sc in cls.__subclasses__():
+                if sc._check_args(*args, **kw):
+                    passlist.append(sc)
+
+            if len(passlist) == 0:
+                raise ValueError("No subclass supports the supplied arguments.")
+            if len(passlist) > 1:
+                raise ValueError("Degenerate subclasses found for the supplied arguments.")
+
+            return passlist[0](*args, **kw)
+
+        # Otherwise create instance
+        else:
+            return type.__call__(cls, *args, **kw)
+
