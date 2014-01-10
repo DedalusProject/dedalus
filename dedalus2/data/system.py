1a4186bf2c90360434d52cff226f0fa782cdd280
Keaton J. Burns <keaton.burns@gmail.com>
2014-01-10 13:58:11 -0500

    Add docs to system module
    hg hash: 76f21ced40d43a2eccfb5ad69f22d0e67a2a4f16

--- dedalus2/data/system.py
+++ dedalus2/data/system.py
@@ -1,10 +1,30 @@
+"""
+Class for systems of fields.
 
+"""
 
 from collections import OrderedDict
 
 
 class System:
-    """Collection of fields."""
+    """
+    Collection of fields.
+
+    Parameters
+    ----------
+    field names : list of strings
+        Names of fields, to be used as keys in field dictionary
+    domain : domain object
+        Problem domain
+
+    Attributes
+    ----------
+    n_fields : int
+        Number of fields in system
+    fields : ordered dict
+        Field objects
+
+    """
 
     def __init__(self, field_names, domain):
 
@@ -18,11 +38,8 @@ class System:
         for fn in field_names:
             self.fields[fn] = domain.new_field()
 
-    def __getitem__(self, item):
+    def __getitem__(self, name):
+        """Return field corresponding to specified name."""
 
-        return self.fields[item]
-
-    def __setitem__(self, item, data):
-
-        self.fields[item] = data
+        return self.fields[name]
 
