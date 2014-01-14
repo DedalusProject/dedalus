980149a1aa8cfaff8764ae6c0930a4e22da7de85
Keaton J. Burns <keaton.burns@gmail.com>
2014-01-14 10:44:44 -0500

    Add logger to public interface
    hg hash: 5eb8b17ee9e3ae551a9c28c3988cea3789d70121

--- dedalus2/public.py
+++ dedalus2/public.py
@@ -3,6 +3,7 @@ Interface for tools typically accessed for solving a problem.
 
 """
 
+from .tools.logging import logger
 from .data.domain import Domain
 from .data import operators
 from .pde.basis import Chebyshev, Fourier
