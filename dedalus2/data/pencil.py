ea2200f95899ad314d11b6d08245072cdebafdb3
Keaton J. Burns <keaton.burns@gmail.com>
2014-01-31 15:37:27 -0500

    Large structural changes moving contiguous storage from pencilset to system and rewriting pencil matrix construction.  These changes are necessary to support conditional equation assignment by pencil.  Work in progress.
    hg hash: 71b408e655cdb910c36825548d727c7205831b29

--- dedalus2/data/pencil.py
+++ dedalus2/data/pencil.py
@@ -3,6 +3,7 @@ Classes for manipulating pencils.
 
 """
 
+from functools import partial
 import numpy as np
 from scipy import sparse
 
@@ -12,205 +13,149 @@ from ..tools.array import expand_pattern
 
 class PencilSet:
     """
-    Pencil system with adjascent memory for efficient computations.
+    Set of pencils in a domain.
+
+    Parameters
+    ----------
+    domain : domain object
+        Problem domain
 
     Attributes
     ----------
-    data : ndarray
-        Contiguous array for system-wide coefficient data concatenated along
-        last axis.
     pencils : list of pencil objects
         Individual pencils
 
     """
 
-    def __init__(self, domain, n_fields):
-
-        # Extend coefficient data shape for system-wide data set
-        shape = np.copy(domain.distributor.coeff_layout.shape)
-        self.stride = shape[-1]
-        shape[-1] *= n_fields
-
-        # Allocate data
-        dtype = domain.distributor.coeff_layout.dtype
-        self.data = np.zeros(shape, dtype=dtype)
-
-        # Build pencils
-        self.pencils = []
-        slice_list, dtrans_list = self._construct_pencil_info(domain)
-        for s, d in zip(slice_list, dtrans_list):
-            pencil = Pencil(self.data, s, d)
-            self.pencils.append(pencil)
-
-    def get_system(self, system):
-        """Copy fields into contiguous pencil buffer."""
-
-        for i, field in enumerate(system.fields.values()):
-            start = i
-            stride = system.n_fields
-
-            field.require_coeff_space()
-            np.copyto(self.data[..., start::stride], field.data)
-
-    def set_system(self, system):
-        """Extract fields from contiguous pencil buffer."""
-
-        for i, field in enumerate(system.fields.values()):
-            start = i
-            stride = system.n_fields
-
-            field.layout = field.domain.distributor.coeff_layout
-            np.copyto(field.data, self.data[..., start::stride])
-
-    def _construct_pencil_info(self, domain):
-        """Construct slice and dtrans lists for each pencil in set."""
+    def __init__(self, domain):
 
         # Get transverse indeces in fastest sequence
         index_list = []
         if domain.dim == 1:
             index_list.append([])
         else:
-            trans_shape = self.data.shape[:-1]
+            trans_shape = domain.distributor.coeff_layout.shape[:-1]
             div = np.arange(np.prod(trans_shape))
             for s in reversed(trans_shape):
                 div, mod = divmod(div, s)
                 index_list.append(mod)
             index_list = list(zip(*reversed(index_list)))
 
-        # Construct corresponding slice and dtrans lists
-        slice_list = []
-        dtrans_list = []
+        # Construct corresponding trans diff consts and build pencils
+        self.pencils = []
         start = domain.distributor.coeff_layout.start
-        for pencil_index in index_list:
-            pencil_slices = []
-            pencil_dtrans = []
-            for n, b in enumerate(domain.bases[:-1]):
-                i = pencil_index[n]
-                pencil_slices.append(slice(i, i+1))
-                pencil_dtrans.append(b.trans_diff(start[n]+i))
-            # Add empty slice for last dimension
-            pencil_slices.append(slice(None))
-            slice_list.append(pencil_slices)
-            dtrans_list.append(pencil_dtrans)
-
-        return slice_list, dtrans_list
+        for index in index_list:
+            trans = []
+            for i, b in enumerate(domain.bases[:-1]):
+                trans.append(b.trans_diff(start[i]+index[i]))
+            self.pencils.append(Pencil(index, trans))
 
 
 class Pencil:
     """
-    Pencil holding problem matrices for a given transverse wavevector.
+    Object holding problem matrices for a given transverse wavevector.
 
     Parameters
     ----------
-    set_data : ndarray
-        Array of pencil set data
-    slice : list of slice objects
-        Slices for retrieving pencil from set data
-    d_trans :  list of floats
-        Perpendicular differentiation constants
-
-    Attributes
-    ----------
-    data : ndarray
-        View of corresponding data in pencil set
+    index : tuple of ints
+        Transverse indeces for retrieving pencil from system data
+    trans :  tuple of floats
+        Transverse differentiation constants
 
     """
 
-    def __init__(self, set_data, slice, d_trans):
+    def __init__(self, index, trans):
 
         # Initial attributes
-        self.set_data = set_data
-        self.slice = slice
-        self.d_trans = d_trans
-        self.data = set_data[slice].squeeze()
+        # Save index as tuple for proper array indexing behavior
+        self.index = tuple(index)
+        self.trans = trans
 
     def build_matrices(self, problem, basis):
-        """Construct PDE matrices from problem and basis matrices."""
+        """Construct pencil matrices from problem and basis matrices."""
 
-        # Size
-        size = problem.size * basis.coeff_size
+        # References
+        size = problem.nfields * basis.coeff_size
         dtype = basis.coeff_dtype
-        D = self.d_trans
 
-        # Problem matrices
-        ML = problem.ML(self.d_trans)
-        MR = problem.MR(self.d_trans)
-        MI = problem.MI(self.d_trans)
-        LL = problem.LL(self.d_trans)
-        LR = problem.LR(self.d_trans)
-        LI = problem.LI(self.d_trans)
+        # Get and unpack problem matrices
+        eqn_mat, bc_mat, S_mat = problem.build_matrices(self.trans)
+        M0e, M1e, L0e, L1e = eqn_mat
+        M0b, M1b, L0b, L1b = bc_mat
+        Se, Sl, Sr, Si = S_mat
+
+        # Use scipy sparse kronecker product with CSR output
+        kron = partial(sparse.kron, format='csr')
 
         # Allocate PDE matrices
-        M = sparse.csr_matrix((size, size), dtype=dtype)
-        L = sparse.csr_matrix((size, size), dtype=dtype)
+        Me = sparse.csr_matrix((size, size), dtype=dtype)
+        Le = sparse.csr_matrix((size, size), dtype=dtype)
 
         # Add terms to PDE matrices
         for i in range(problem.order):
-            Pre_i = basis.Pre * basis.Mult(i)
-            Diff_i = basis.Pre * basis.Mult(i) * basis.Diff
+            if i > 0:
+                raise ValueError("Nonconstant coefficients not supported.")
+            PM  = basis.Pre * basis.Mult(i)
+            PMD = basis.Pre * basis.Mult(i) * basis.Diff
+            Me = Me + kron(PM,  Se*M0e)
+            Me = Me + kron(PMD, Se*M1e)
+            Le = Le + kron(PM,  Se*L0e)
+            Le = Le + kron(PMD, Se*L1e)
 
-            M = M + sparse.kron(Pre_i, problem.M0[i](D), format='csr')
-            M = M + sparse.kron(Diff_i, problem.M1[i](D), format='csr')
-            L = L + sparse.kron(Pre_i, problem.L0[i](D), format='csr')
-            L = L + sparse.kron(Diff_i, problem.L1[i](D), format='csr')
-
-        # Allocate boundary condition matrices
+        # Allocate BC matrices
         Mb = sparse.csr_matrix((size, size), dtype=dtype)
         Lb = sparse.csr_matrix((size, size), dtype=dtype)
 
-        # Add terms to boundary condition matrices
-        if np.any(ML):
-            Mb = Mb + sparse.kron(basis.Left, ML, format='csr')
-        if np.any(MR):
-            Mb = Mb + sparse.kron(basis.Right, MR, format='csr')
-        if np.any(MI):
-            Mb = Mb + sparse.kron(basis.Int, MI, format='csr')
-        if np.any(LL):
-            Lb = Lb + sparse.kron(basis.Left, LL, format='csr')
-        if np.any(LR):
-            Lb = Lb + sparse.kron(basis.Right, LR, format='csr')
-        if np.any(LI):
-            Lb = Lb + sparse.kron(basis.Int, LI, format='csr')
+        # Add terms to BC matrices
+        if Sl.any():
+            for i in range(problem.order):
+                LM  = basis.Left * basis.Mult(i)
+                LMD = basis.Left * basis.Mult(i) * basis.Diff
+                Mb = Mb + kron(LM,  Sl*M0b)
+                Mb = Mb + kron(LMD, Sl*M1b)
+                Lb = Lb + kron(LM,  Sl*L0b)
+                Lb = Lb + kron(LMD, Sl*L1b)
+        if Sr.any():
+            for i in range(problem.order):
+                RM  = basis.Right * basis.Mult(i)
+                RMD = basis.Right * basis.Mult(i) * basis.Diff
+                Mb = Mb + kron(RM,  Sr*M0b)
+                Mb = Mb + kron(RMD, Sr*M1b)
+                Lb = Lb + kron(RM,  Sr*L0b)
+                Lb = Lb + kron(RMD, Sr*L1b)
+        if Si.any():
+            for i in range(problem.order):
+                IM  = basis.Int * basis.Mult(i)
+                IMD = basis.Int * basis.Mult(i) * basis.Diff
+                Mb = Mb + kron(IM,  Si*M0b)
+                Mb = Mb + kron(IMD, Si*M1b)
+                Lb = Lb + kron(IM,  Si*L0b)
+                Lb = Lb + kron(IMD, Si*L1b)
 
-        # Get set of boundary condition rows
+        # Build filter matrix to eliminate boundary condition rows
         Mb_rows = Mb.nonzero()[0]
         Lb_rows = Lb.nonzero()[0]
         rows = set(Mb_rows).union(set(Lb_rows))
-
-        # Clear boundary condition rows in PDE matrices
-        clear_bc = sparse.eye(size, dtype=dtype, format='dok')
+        F = sparse.eye(size, dtype=dtype, format='dok')
         for i in rows:
-            clear_bc[i, i] = 0.
+            F[i, i] = 0.
+        F = F.tocsr()
 
-        clear_bc = clear_bc.tocsr()
-        M = M.tocsr()
-        L = L.tocsr()
-
-        M = clear_bc * M
-        L = clear_bc * L
-
-        # Add boundary condition terms to PDE matrices
-        M = M + Mb
-        L = L + Lb
+        # Combine filtered PDE matrices with BC matrices
+        M = F*Me + Mb
+        L = F*Le + Lb
 
         # Store with expanded sparsity for fast combination during integration
         self.LHS = zeros_with_pattern(M, L).tocsr()
         self.M = expand_pattern(M, self.LHS).tocsr()
         self.L = expand_pattern(L, self.LHS).tocsr()
 
-        # Reference nonlinear expressions
-        self.F = problem.F
-        self.F_eval = sparse.kron(basis.Pre, np.eye(problem.size))
-        b = np.kron(basis.bc_vector[:,0], problem.b(D))
-        self.bc_f = [b[r] for r in rows]
-        self.bc_rows = list(rows)
-        self.parameters = problem.parameters
-
-        # UPGRADE: Caste boundary conditions as functionals on operator trees
-        # self.BL = problem.BL
-        # self.BR = problem.BR
-        # self.BI = problem.BI
-        # self.BL_eval = sparse.kron(np.eye(problem.size), basis.Left)
-        # self.BR_eval = sparse.kron(np.eye(problem.size), basis.Right)
-        # self.BI_eval = sparse.kron(np.eye(problem.size), basis.Int)
+        # Store selection/restriction operators for RHS
+        # Start G_bc with integral term, since that matrix is always defined
+        self.G_eq = F * kron(basis.Pre, Se)
+        self.G_bc = kron(basis.Int, Si)
+        if Sl.any():
+            self.G_bc = self.G_bc + kron(L, Sl)
+        if Sr.any():
+            self.G_bc = self.G_bc + kron(R, Sr)
 
--- dedalus2/data/system.py
+++ dedalus2/data/system.py
@@ -4,42 +4,89 @@ Class for systems of fields.
 """
 
 from collections import OrderedDict
+import numpy as np
 
 
 class System:
     """
-    Collection of fields.
+    Collection of fields with continguous internal buffer for efficient
+    computations.
 
     Parameters
     ----------
-    field names : list of strings
+    field_names : list of strings
         Names of fields, to be used as keys in field dictionary
     domain : domain object
         Problem domain
 
     Attributes
     ----------
-    n_fields : int
-        Number of fields in system
+    data : ndarray
+        Contiguous buffer for field
     fields : ordered dict
         Field objects
+    nfields : int
+        Number of fields in system
 
     """
 
     def __init__(self, field_names, domain):
 
-        # Initial attributes
-        self.field_names = field_names
-        self.n_fields = len(field_names)
-        self.domain = domain
+        # Handle nameless input
+        if np.isscalar(field_names):
+            nfields = field_names
+            field_names = [i for i in range(nfields)]
+        else:
+            nfields = len(field_names)
 
         # Build fields
-        self.fields = OrderedDict()
+        fields = OrderedDict()
         for fn in field_names:
-            self.fields[fn] = domain.new_field()
+            fields[fn] = domain.new_field()
+
+        # Allocate data of extended coeff data shape
+        coeff_layout = domain.distributor.coeff_layout
+        shape = np.copy(coeff_layout.shape)
+        shape[-1] *= nfields
+        self.data = np.zeros(shape, dtype=coeff_layout.dtype)
+
+        # References
+        self.fields = fields
+        self.nfields = nfields
+        self._coeff_layout = coeff_layout
 
     def __getitem__(self, name):
         """Return field corresponding to specified name."""
 
         return self.fields[name]
+
+    def gather(self):
+        """Copy fields into system buffer."""
+
+        for i, field in enumerate(self.fields.values()):
+            start = i
+            stride = self.nfields
+
+            field.require_coeff_space()
+            np.copyto(self.data[..., start::stride], field.data)
+
+    def scatter(self):
+        """Extract fields from system buffer."""
+
+        for i, field in enumerate(self.fields.values()):
+            start = i
+            stride = self.nfields
+
+            field.layout = self._coeff_layout
+            np.copyto(field.data, self.data[..., start::stride])
+
+    def get_pencil(self, pencil):
+        """Return pencil from system buffer."""
+
+        return self.data[pencil.indeces]
+
+    def set_pencil(self, pencil, data):
+        """Set pencil in system buffer."""
+
+        np.copyto(self.data[pencil.indeces], data)
 
