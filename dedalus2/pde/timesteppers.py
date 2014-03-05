56c9abeee171a33c5ed9386bb745ec1775840722
Keaton J. Burns <keaton.burns@gmail.com>
2014-03-05 01:40:50 -0500

    Reorganize framework for multistep IMEX methods, making them easier to program and more in-line with the syntax of Wang 2008.  Implement all their 2nd order IMEX schemes: SBDF2, CNAB, MCNAB, CNLF
    hg hash: 15d56078b54122137181c5a3c31bce8e7accb9b6

--- dedalus2/pde/timesteppers.py
+++ dedalus2/pde/timesteppers.py
@@ -9,9 +9,9 @@ import numpy as np
 from ..data.system import CoeffSystem, FieldSystem
 
 
-class IMEXBase:
+class MultistepIMEX:
     """
-    Base class for implicit-explicit timesteppers.
+    Base class for implicit-explicit multistep methods.
 
     Parameters
     ----------
@@ -25,8 +25,22 @@ class IMEXBase:
     These timesteppers discretize the system
         M.dt(X) + L.X = F
     into the general form
-        ai M.X(n+1) + bi L.X(n+1) = aj M.X(n-j) + bj L.X(n-j) + cj F(n-j)
-    where j runs from 0 to {amax-1, bmax-1, cmax-1} in each sum, respectively.
+        aj M.X(n-j) + bj L.X(n-j) = cj F(n-j)
+    where j runs from {0, 0, 1} to {amax, bmax, cmax}.
+
+    The system is then solved as
+        (a0 M + b0 L).X(n) = cj F(n-j) - aj M.X(n-j) - bj L.X(n-j)
+    where j runs from {1, 1, 1} to {cmax, amax, bmax}.
+
+    References
+    ----------
+    D. Wang and S. J. Ruuth, Journal of Computational Mathematics 26, (2008).*
+
+    * Our coefficients are related to those used by Wang as:
+        amax = bmax = cmax = s
+        aj = α(s-j) / k(n+s-1)
+        bj = γ(s-j)
+        cj = β(s-j)
 
     """
 
@@ -36,7 +50,7 @@ class IMEXBase:
         N = max(self.amax, self.bmax, self.cmax)
         self.dt = deque([0.]*N)
 
-        # Create systems for multistep history
+        # Create coefficient systems for multistep history
         self.MX = MX = deque()
         self.LX = LX = deque()
         self.F = F = deque()
@@ -63,7 +77,7 @@ class IMEXBase:
         self.dt[0] = dt
 
         # Compute IMEX coefficients
-        ai, bi, a, b, c = self.compute_coefficients(self._iteration)
+        a, b, c = self.compute_coefficients(self.dt, self._iteration)
         self._iteration += 1
 
         # Update RHS components and LHS matrices
@@ -74,6 +88,8 @@ class IMEXBase:
         MX0 = MX[0]
         LX0 = LX[0]
         F0 = F[0]
+        a0 = a[0]
+        b0 = b[0]
         for p in pencils:
             x = state.get_pencil(p)
             pFe = Fe.get_pencil(p)
@@ -83,47 +99,165 @@ class IMEXBase:
             LX0.set_pencil(p, p.L*x)
             F0.set_pencil(p, p.G_eq*pFe + p.G_bc*pFb)
 
-            np.copyto(p.LHS.data, ai*p.M.data + bi*p.L.data)
+            np.copyto(p.LHS.data, a0*p.M.data + b0*p.L.data)
 
         # Build RHS
         RHS.data.fill(0)
-        for j in range(self.amax):
-            RHS.data += a[j] * MX[j].data
-        for j in range(self.bmax):
-            RHS.data += b[j] * LX[j].data
-        for j in range(self.cmax):
-            RHS.data += c[j] * F[j].data
+        for j in range(1, len(c)):
+            RHS.data += c[j] * F[j-1].data
+        for j in range(1, len(a)):
+            RHS.data -= a[j] * MX[j-1].data
+        for j in range(1, len(b)):
+            RHS.data -= b[j] * LX[j-1].data
 
 
-class Euler(IMEXBase):
-    """Backward-Euler/Forward-Euler"""
+class Euler(MultistepIMEX):
+    """First-order forward/backward Euler [Wang 2008 eqn 2.6]"""
 
     amax = 1
     bmax = 1
     cmax = 1
 
-    def compute_coefficients(self, iteration):
+    @classmethod
+    def compute_coefficients(self, timesteps, iteration):
 
-        a = [0.] * self.amax
-        b = [0.] * self.bmax
-        c = [0.] * self.cmax
+        a = np.zeros(self.amax+1)
+        b = np.zeros(self.bmax+1)
+        c = np.zeros(self.cmax+1)
 
-        # References
-        dt = self.dt[0]
+        k1, *rest = timesteps
 
-        # LHS coefficients
-        ai = 1.
-        bi = dt
+        a[0] = 1 / k1
+        a[1] = -1 / k1
+        b[0] = 1
+        c[1] = 1
 
-        # RHS coefficients
-        a[0] = 1.
-        b[0] = 0.
-        c[0] = dt
-
-        return ai, bi, a, b, c
+        return a, b, c
 
 
-class CNAB3(IMEXBase):
+class SBDF2(MultistepIMEX):
+    """Second-order semi-implicit BDF [Wang 2008 eqn 2.8]"""
+
+    amax = 2
+    bmax = 2
+    cmax = 2
+
+    @classmethod
+    def compute_coefficients(self, timesteps, iteration):
+
+        if iteration < 1:
+            return Euler.compute_coefficients(timesteps, iteration)
+
+        a = np.zeros(self.amax+1)
+        b = np.zeros(self.bmax+1)
+        c = np.zeros(self.cmax+1)
+
+        k1, k2, *rest = timesteps
+        w1 = k1 / k2
+
+        a[0] = (1 + 2*w1) / (1 + w1) / k1
+        a[1] = -(1 + w1) / k1
+        a[2] = w1*w1 / (1 + w1) / k1
+        b[0] = 1
+        c[1] = 1 + w1
+        c[2] = -w1
+
+        return a, b, c
+
+
+class CNAB(MultistepIMEX):
+    """Second-order Crank-Nicolson-Adams-Bashforth [Wang 2008 eqn 2.9]"""
+
+    amax = 2
+    bmax = 2
+    cmax = 2
+
+    @classmethod
+    def compute_coefficients(self, timesteps, iteration):
+
+        if iteration < 1:
+            return Euler.compute_coefficients(timesteps, iteration)
+
+        a = np.zeros(self.amax+1)
+        b = np.zeros(self.bmax+1)
+        c = np.zeros(self.cmax+1)
+
+        k1, k2, *rest = timesteps
+        w1 = k1 / k2
+
+        a[0] = 1 / k1
+        a[1] = -1 / k1
+        b[0] = 1 / 2
+        b[1] = 1 / 2
+        c[1] = 1 + w1 / 2
+        c[2] = -w1 / 2
+
+        return a, b, c
+
+
+class MCNAB(MultistepIMEX):
+    """Second-order modified Crank-Nicolson-Adams-Bashforth [Wang 2008 eqn 2.10]"""
+
+    amax = 2
+    bmax = 2
+    cmax = 2
+
+    @classmethod
+    def compute_coefficients(self, timesteps, iteration):
+
+        if iteration < 1:
+            return Euler.compute_coefficients(timesteps, iteration)
+
+        a = np.zeros(self.amax+1)
+        b = np.zeros(self.bmax+1)
+        c = np.zeros(self.cmax+1)
+
+        k1, k2, *rest = timesteps
+        w1 = k1 / k2
+
+        a[0] = 1 / k1
+        a[1] = -1 / k1
+        b[0] = (8 + 1/w1) / 16
+        b[1] = (7 - 1/w1) / 16
+        b[2] = 1 / 16
+        c[1] = 1 + w1 / 2
+        c[2] = -w1 / 2
+
+        return a, b, c
+
+
+class CNLF(MultistepIMEX):
+    """Second-order Crank-Nicolson-Leap-Frog [Wang 2008 eqn 2.11]"""
+
+    amax = 2
+    bmax = 2
+    cmax = 2
+
+    @classmethod
+    def compute_coefficients(self, timesteps, iteration):
+
+        if iteration < 1:
+            return Euler.compute_coefficients(timesteps, iteration)
+
+        a = np.zeros(self.amax+1)
+        b = np.zeros(self.bmax+1)
+        c = np.zeros(self.cmax+1)
+
+        k1, k2, *rest = timesteps
+        w1 = k1 / k2
+
+        a[0] = 1 / (1 + w1) / k1
+        a[1] = (w1 - 1) / k1
+        a[2] = -w1*w1 / (1 + w1) / k1
+        b[0] = 1 / w1 / 2
+        b[1] = (1 - 1/w1) / 2
+        b[2] = 1 / 2
+        c[1] = 1
+
+        return a, b, c
+
+
+class CNAB3(MultistepIMEX):
     """Third order Crank-Nicolson-Adams-Bashforth"""
 
     amax = 1
@@ -162,46 +296,6 @@ class CNAB3(IMEXBase):
         c[0] *= dt0
         c[1] *= dt0
         c[2] *= dt0
-
-        return ai, bi, a, b, c
-
-
-class MCNAB2(IMEXBase):
-    """Second order Modified Crank-Nicolson-Adams-Bashforth with variable timestep size (VSIMEX)"""
-
-    amax = 1
-    bmax = 2
-    cmax = 2
-
-    def compute_coefficients(self, iteration):
-
-        a = [0.] * self.amax
-        b = [0.] * self.bmax
-        c = [0.] * self.cmax
-
-        # References
-        dt0 = self.dt[0]
-        dt1 = self.dt[1]
-
-        # LHS coefficients
-        ai = 1.
-        bi = dt0 * (1./16.)*(8. + (dt1/dt0))
-
-        # RHS coefficients
-        a[0] = 1.
-
-        if iteration == 0:
-            b[0] = -dt0 / 2.
-            c[0] = 1.
-
-        else:
-            b[0] = -dt0 * (1./16.)*(7. - (dt1/dt0))
-            b[1] = -dt0 * (1./16.)
-            c[1] = -1./2. * (dt0 / dt1)
-            c[0] = 1. + 1./2. *(dt0 / dt1)
-
-        c[0] *= dt0
-        c[1] *= dt0
 
         return ai, bi, a, b, c
 
