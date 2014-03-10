bcc48acd55a1238f43181961230086b4214dc769
Keaton J. Burns <keaton.burns@gmail.com>
2014-03-10 00:21:54 -0400

    Change timestepper update_pencils to step methods that handle evaluation and solves.  Add Runge Kutta IMEX class and 2 integrators.
    hg hash: e4dfd938dc21f5200571858492e2900b82bb7c5d

--- dedalus2/pde/timesteppers.py
+++ dedalus2/pde/timesteppers.py
@@ -5,6 +5,7 @@ ODE solvers for timestepping.
 
 from collections import deque
 import numpy as np
+from scipy.sparse import linalg
 
 from ..data.system import CoeffSystem, FieldSystem
 
@@ -46,6 +47,8 @@ class MultistepIMEX:
 
     def __init__(self, nfields, domain):
 
+        self.RHS = CoeffSystem(nfields, domain)
+
         # Create deque for storing recent timesteps
         N = max(self.amax, self.bmax, self.cmax)
         self.dt = deque([0.]*N)
@@ -64,13 +67,23 @@ class MultistepIMEX:
         # Attributes
         self._iteration = 0
 
-    def update_pencils(self, pencils, state, RHS, Fe, Fb, dt):
-        """Compute elements for the implicit solve, by pencil."""
+    def step(self, solver, dt, wall_time):
+        """Advance solver by one timestep."""
+
+        # Solver references
+        pencils = solver.pencils
+        evaluator = solver.evaluator
+        state = solver.state
+        Fe = solver.Fe
+        Fb = solver.Fb
+        sim_time = solver.sim_time
+        iteration = solver.iteration
 
         # References
         MX = self.MX
         LX = self.LX
         F = self.F
+        RHS = self.RHS
 
         # Cycle and compute timesteps
         self.dt.rotate()
@@ -79,6 +92,10 @@ class MultistepIMEX:
         # Compute IMEX coefficients
         a, b, c = self.compute_coefficients(self.dt, self._iteration)
         self._iteration += 1
+
+        # Run evaluator
+        state.scatter()
+        evaluator.evaluate_scheduled(wall_time, sim_time, iteration)
 
         # Update RHS components and LHS matrices
         MX.rotate()
@@ -90,6 +107,7 @@ class MultistepIMEX:
         F0 = F[0]
         a0 = a[0]
         b0 = b[0]
+
         for p in pencils:
             x = state.get_pencil(p)
             pFe = Fe.get_pencil(p)
@@ -109,6 +127,16 @@ class MultistepIMEX:
             RHS.data -= a[j] * MX[j-1].data
         for j in range(1, len(b)):
             RHS.data -= b[j] * LX[j-1].data
+
+        # Solve
+        for p in pencils:
+            A = p.LHS
+            b = RHS.get_pencil(p)
+            x = linalg.spsolve(A, b, use_umfpack=False, permc_spec='NATURAL')
+            state.set_pencil(p, x)
+
+        # Update solver
+        solver.sim_time += dt
 
 
 class CNAB1(MultistepIMEX):
@@ -401,4 +429,154 @@ class SBDF4(MultistepIMEX):
         c[4] = -w1**3 * w2**2 * w3 * (1 + w3) / (1 + w1) * A2 / A1
 
         return a, b, c
+
+
+class RungeKuttaIMEX:
+    """
+    Base class for implicit-explicit multistep methods.
+
+    Parameters
+    ----------
+    nfields : int
+        Number of fields in problem
+    domain : domain object
+        Problem domain
+
+    Notes
+    -----
+    These timesteppers discretize the system
+        M.dt(X) + L.X = F
+    by constructing s stages
+        M.X(n,i) - M.X(n,0) + k Hij L.X(n,j) = k Aij F(n,j)
+    where j runs from {0, 0} to {i, i-1}, and F is evaluated at time
+        t(n,i) = t(n,0) + k ci
+
+    The s stages are solved as
+        (M + k Hii L).X(n,i) = M.X(n,0) + k Aij F(n,j) - k Hij L.X(n,j)
+    where j runs from {0, 0} to {i-1, i-1}.
+
+    The final stage is used as the advanced solution*:
+        X(n+1,0) = X(n,s)
+        t(n+1,0) = t(n,s) = t(n,0) + k
+
+    * Equivalently the Butcher tableaus must follow
+        b_im = H[s, :]
+        b_ex = A[s, :]
+        c[s] = 1
+
+    References
+    ----------
+    Ascher
+
+    """
+
+    def __init__(self, nfields, domain):
+
+        self.RHS = CoeffSystem(nfields, domain)
+
+        # Create coefficient systems for multistep history
+        self.MX0 = CoeffSystem(nfields, domain)
+        self.LX = LX = deque()
+        self.F = F = deque()
+        for j in range(self.stages):
+            LX.append(CoeffSystem(nfields, domain))
+            F.append(CoeffSystem(nfields, domain))
+
+    def step(self, solver, dt, wall_time):
+        """Advance solver by one timestep."""
+
+        # Solver references
+        pencils = solver.pencils
+        evaluator = solver.evaluator
+        state = solver.state
+        Fe = solver.Fe
+        Fb = solver.Fb
+        sim_time_0 = solver.sim_time
+        iteration = solver.iteration
+
+        # Other references
+        RHS = self.RHS
+        MX0 = self.MX0
+        LX = self.LX
+        F = self.F
+        A = self.A
+        H = self.H
+        c = self.c
+        k = dt
+
+        # Compute M.X(n,0)
+        for p in pencils:
+            x0 = state.get_pencil(p)
+            MX0.set_pencil(p, p.M*x0)
+
+        # Compute stages
+        # (M + k Hii L).X(n,i) = M.X(n,0) + k Aij F(n,j) - k Hij L.X(n,j)
+        for i in range(1, self.stages+1):
+
+            # Compute F(n,i-1), L.X(n,i-1)
+            solver.sim_time = sim_time_0 + k*c[i]
+            state.scatter()
+            if i == 1:
+                evaluator.evaluate_scheduled(wall_time, solver.sim_time, iteration)
+            else:
+                evaluator.evaluate_group('F', wall_time, solver.sim_time, iteration)
+            for p in pencils:
+                x = state.get_pencil(p)
+                pFe = Fe.get_pencil(p)
+                pFb = Fb.get_pencil(p)
+                LX[i-1].set_pencil(p, p.L*x)
+                F[i-1].set_pencil(p, p.G_eq*pFe + p.G_bc*pFb)
+
+            # Construct RHS(i)
+            np.copyto(RHS.data, MX0.data)
+            for j in range(0, i):
+                RHS.data += k * A[i,j] * F[j].data
+                RHS.data -= k * H[i,j] * LX[j].data
+
+            # Solve for X(n,i)
+            for p in pencils:
+                np.copyto(p.LHS.data, p.M.data + (k*H[i,i])*p.L.data)
+                A = p.LHS
+                b = RHS.get_pencil(p)
+                x = linalg.spsolve(A, b, use_umfpack=False, permc_spec='NATURAL')
+                state.set_pencil(p, x)
+
+
+class RK222(RungeKuttaIMEX):
+    """2nd-order 2-stage DIRK+ERK scheme [Ascher 1997 sec 2.6]"""
+
+    stages = 2
+
+    γ = (2 - np.sqrt(2)) / 2
+    δ = 1 - 1 / γ / 2
+
+    c = np.array([0, γ, 1])
+
+    A = np.array([[0,  0 , 0],
+                  [γ,  0 , 0],
+                  [δ, 1-δ, 0]])
+
+    H = np.array([[0,  0 , 0],
+                  [0,  γ , 0],
+                  [0, 1-γ, γ]])
+
+
+class RK443(RungeKuttaIMEX):
+    """3rd-order 4-stage DIRK+ERK scheme [Ascher 1997 sec 2.8]"""
+
+    stages = 4
+
+    c = np.array([0, 1/2, 2/3, 1/2, 1])
+
+    A = np.array([[  0  ,   0  ,  0 ,   0 , 0],
+                  [ 1/2 ,   0  ,  0 ,   0 , 0],
+                  [11/18,  1/18,  0 ,   0 , 0],
+                  [ 5/6 , -5/6 , 1/2,   0 , 0],
+                  [ 1/4 ,  7/4 , 3/4, -7/4, 0]])
+
+    H = np.array([[0,   0 ,   0 ,  0 ,  0 ],
+                  [0,  1/2,   0 ,  0 ,  0 ],
+                  [0,  1/6,  1/2,  0 ,  0 ],
+                  [0, -1/2,  1/2, 1/2,  0 ],
+                  [0,  3/2, -3/2, 1/2, 1/2]])
 
