af0f8b25d1a62c03b327f4c1ee424e9d48267384
Keaton J. Burns <keaton.burns@gmail.com>
2014-03-14 20:51:30 -0400

    Fix import order for public interface
    hg hash: be8ff57350fc4c87bd0b20f9c3aef60edb2e4958

--- dedalus2/public.py
+++ dedalus2/public.py
@@ -3,6 +3,7 @@ Interface for tools typically accessed for solving a problem.
 
 """
 
+from .data import future as _future
 from .tools.logging import logger
 from .data.domain import Domain
 from .data import operators
