ce76e1f28625871097beac29cc789dac040dcb73
Benjamin Brown <bpbrown@gmail.com>
2014-04-30 22:58:25 -0700

    90% of the way to using a proper parent object.
    hg hash: 3092975aeb95feec596e8b82c2a63301b47c2cf6

--- atmospheres/atmospheres.py
+++ atmospheres/atmospheres.py
@@ -2,69 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dedalus2.public as d2
 
-class polytrope():#dedalus_atmosphere):
-    def __init__(self, gamma, polytropic_index, z0, z):
-        #dedalus_atmosphere.__init__(**args)
-        self.gamma = gamma
-        
-        self.polytropic_index = polytropic_index
-        self.polytropic_index_ad = 1/(self.gamma-1)
-        # shorthand versions
-        self.n = self.polytropic_index
-        self.n_ad = self.polytropic_index_ad
+class DedalusAtmosphere():
+    def __init__(self, z_basis, num_coeffs=20):
 
-        self.epsilon = self.polytropic_index - self.polytropic_index_ad
-        print("ε = ", self.epsilon)
-
-        self.z0 = z0
-        self.parameters = dict()
-        self.parameters['grad_ln_rho'] = self.grad_ln_rho
-        self.parameters['grad_S'] = self.grad_S
-
-        self.set_grid(z.grid)
-        
-    def set_grid(self, z):
-        self.z = z
-        
-    def grad_ln_rho(self):
-        return -self.polytropic_index/(self.z0-self.z)
-
-    def grad_S(self):
-        entropy_gradient_factor = ((self.polytropic_index+1)/self.gamma - self.polytropic_index)
-        print(entropy_gradient_factor)
-        print(self.epsilon/self.gamma)
-        return -entropy_gradient_factor/(self.z0-self.z)
-
-    def grad_ln_T(self):
-        return -1/(self.z0-self.z)
-
-    def rho(self):
-        return (self.z0-self.z)**self.polytropic_index
-
-    def T(self):
-        return (self.z0-self.z)
-
-    def P(self):
-        return (self.z0-self.z)**(self.polytropic_index+1)
-
-    def S(self):
-        return 
-    
-class dedalus_atmosphere():
-    def __init__(self, z_basis, atmosphere, num_coeffs=20):
-        self.atmosphere = atmosphere
         self.num_coeffs = num_coeffs
         self.domain = d2.Domain([z_basis])
         
         self.z_atmosphere = z_basis.grid
         self.z = z_basis.grid
 
-        self.grad_ln_rho_atmosphere = self.domain.new_field()
-        self.grad_S_atmosphere = self.domain.new_field()
-
-        self.atmosphere.set_grid(self.z_atmosphere)
-        
-        self.set_atmosphere()
+        self.fields = dict()
+        keys = sorted(self.parameters.keys())
+        for key in keys:
+            print("setting {:s}".format(key))
+            self.fields[key] = self.domain.new_field()
+            self.fields[key]['g'] =  self.parameters[key]()
 
         self.check_spectrum()
         print("Atmosphere spectrum has been output to: atmosphere_spectrum.png")
@@ -73,11 +25,7 @@ class dedalus_atmosphere():
 
         self.check_atmosphere()
         print("Atmosphere comparison has been output to: atmosphere.png")
-        
-    def set_atmosphere(self):
-        self.grad_ln_rho_atmosphere['g'] = self.atmosphere.parameters['grad_ln_rho']()
-        self.grad_S_atmosphere['g'] = self.atmosphere.parameters['grad_S']()
-                
+                        
     def truncate_atmosphere(self):
         self.grad_ln_rho_atmosphere['c'][self.num_coeffs:] = 0
         self.grad_S_atmosphere['c'][self.num_coeffs:] = 0
@@ -131,6 +79,53 @@ class dedalus_atmosphere():
                  np.abs(self.grad_S()/self.atmosphere.parameters['grad_S']()-1))
         fig.savefig('atmosphere.png')
         plt.close(fig)
+        
+class Polytrope(DedalusAtmosphere):
+    def __init__(self, gamma, polytropic_index, z0, z, **args):
+        
+        self.gamma = gamma
+        
+        self.polytropic_index = polytropic_index
+        self.polytropic_index_ad = 1/(self.gamma-1)
+        # shorthand versions
+        self.n = self.polytropic_index
+        self.n_ad = self.polytropic_index_ad
+
+        self.epsilon = self.polytropic_index - self.polytropic_index_ad
+        print("ε = ", self.epsilon)
+
+        self.z0 = z0
+        self.parameters = dict()
+        self.parameters['grad_ln_rho'] = self.grad_ln_rho
+        self.parameters['grad_S'] = self.grad_S
+
+        DedalusAtmosphere.__init__(self, z, **args)
+                
+    def grad_ln_rho(self):
+        return -self.polytropic_index/(self.z0-self.z)
+
+    def grad_S(self):
+        entropy_gradient_factor = ((self.polytropic_index+1)/self.gamma - self.polytropic_index)
+        print(entropy_gradient_factor)
+        print(self.epsilon/self.gamma)
+        return -entropy_gradient_factor/(self.z0-self.z)
+
+    def grad_ln_T(self):
+        return -1/(self.z0-self.z)
+
+    def rho(self):
+        return (self.z0-self.z)**self.polytropic_index
+
+    def T(self):
+        return (self.z0-self.z)
+
+    def P(self):
+        return (self.z0-self.z)**(self.polytropic_index+1)
+
+    def S(self):
+        return 
+    
+
 
 
 if __name__ == "__main__":
@@ -147,6 +142,4 @@ if __name__ == "__main__":
     
     num_coeffs = 20
 
-    atm = polytrope(gamma, poly_n, Lz+1, z_basis)
-
-    dedalus_atm = dedalus_atmosphere(z_basis, atm, num_coeffs=num_coeffs)
+    atm = Polytrope(gamma, poly_n, Lz+1, z_basis, num_coeffs=num_coeffs)
