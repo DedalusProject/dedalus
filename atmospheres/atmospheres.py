18596dafef8fbf38c3f18c4445b5759cc48d4704
Benjamin Brown <bpbrown@gmail.com>
2014-05-01 00:08:54 -0700

    Added ScaledPolytrope; works (and inherits from polytrope)!
    hg hash: e0717fd220b7b0fadc20cd767b8d1161d9647458

--- atmospheres/atmospheres.py
+++ atmospheres/atmospheres.py
@@ -6,43 +6,50 @@ class DedalusAtmosphere():
     def __init__(self, z_basis, num_coeffs=20):
 
         self.num_coeffs = num_coeffs
-        self.domain = d2.Domain([z_basis])
         
-        self.z_atmosphere = z_basis.grid
+        self.domain = d2.Domain([z_basis])        
         self.z = z_basis.grid
 
-        self.fields = dict()
-        self.keys = sorted(self.parameters.keys())
+        self.atmosphere = dict()
+        self.keys = sorted(self.base_atmosphere.keys())
         self.N_parameters = len(self.keys)
 
         for key in self.keys:
             print("setting {:s}".format(key))
-            self.fields[key] = self.domain.new_field()
-            self.fields[key]['g'] =  self.parameters[key]()
+            self.atmosphere[key] = self.domain.new_field()
+            self.atmosphere[key]['g'] =  self.base_atmosphere[key]()
 
             self.check_spectrum(key)        
             self.truncate_atmosphere(key)
             self.check_atmosphere(key)
                         
     def truncate_atmosphere(self, key):
-        self.fields[key]['c'][self.num_coeffs:] = 0
+        self.atmosphere[key]['c'][self.num_coeffs:] = 0
 
     def get_coeffs(self, key):
-        return np.copy(self.fields[key]['c'][:self.num_coeffs])
+        return np.copy(self.atmosphere[key]['c'][:self.num_coeffs])
     
     def get_values(self, key):
-        return np.copy(self.fields[key]['g'].real)
+        return np.copy(self.atmosphere[key]['g'].real)
 
     
     def check_spectrum(self, key, individual_plots=True):
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
-        ax.loglog(np.abs(self.fields[key]['c']*np.conj(self.fields[key]['c'])))
+        power_spectrum = np.abs(self.atmosphere[key]['c']*np.conj(self.atmosphere[key]['c']))
+        ax.loglog(power_spectrum)
 
         ax.axvline(x=self.num_coeffs,linestyle='dashed')
         ax.set_title(self.titles[key])
         ax.set_xlabel(r"$T_n$")
 
+        sp_bound = (np.max(power_spectrum)*1e-8)
+        dp_bound = (np.max(power_spectrum)*1e-16)
+        
+        ax.axhline(y=sp_bound,linestyle='dashed')
+        ax.axhline(y=dp_bound,linestyle='dashed')
+
+        
         ax.set_ylabel("power spectrum")
 
         atm_file = "spectrum_{:s}.png".format(key)
@@ -56,13 +63,13 @@ class DedalusAtmosphere():
         ax1 = fig.add_subplot(2,1,1)
         ax2 = fig.add_subplot(2,1,2)
 
-        ax1.plot(self.z, self.parameters[key]())
+        ax1.plot(self.z, self.base_atmosphere[key]())
         ax1.plot(self.z, self.get_values(key))
 
         ax1.set_ylabel(self.titles[key])
         ax1.set_title("truncated atmosphere {:s}".format(self.titles[key]))
 
-        ax2.semilogy(self.z, np.abs(self.parameters[key]()/self.get_values(key)-1))
+        ax2.semilogy(self.z, np.abs(self.base_atmosphere[key]()/self.get_values(key)-1))
 
         ax2.set_ylabel("relative error")
         ax2.set_xlabel(r"$z$")
@@ -87,23 +94,24 @@ class Polytrope(DedalusAtmosphere):
 
         self.epsilon = self.polytropic_index - self.polytropic_index_ad
         print("ε = ", self.epsilon)
-
+        print("Nρ = ", self.polytropic_index*np.log(z0))
+              
         self.z0 = z0
 
-        self.parameters = dict()
+        self.base_atmosphere = dict()
         self.titles = dict()
         
-        self.parameters['grad_ln_rho'] = self.grad_ln_rho
+        self.base_atmosphere['grad_ln_rho'] = self.grad_ln_rho
         self.titles['grad_ln_rho'] = r"$\nabla \ln \rho_0$"
-        self.parameters['grad_S'] = self.grad_S
+        self.base_atmosphere['grad_S'] = self.grad_S
         self.titles['grad_S'] = r"$\nabla S_0$"
-        self.parameters['grad_ln_T'] = self.grad_ln_T
+        self.base_atmosphere['grad_ln_T'] = self.grad_ln_T
         self.titles['grad_ln_T'] = r"$\nabla \ln T_0$"
-        self.parameters['rho'] = self.rho
+        self.base_atmosphere['rho'] = self.rho
         self.titles['rho'] = r"$\rho_0$"
-        self.parameters['T'] = self.T
+        self.base_atmosphere['T'] = self.T
         self.titles['T'] = r"$T_0$"
-        self.parameters['P'] = self.P
+        self.base_atmosphere['P'] = self.P
         self.titles['P'] = r"$P_0$"
 
         
@@ -132,14 +140,44 @@ class Polytrope(DedalusAtmosphere):
 
     def S(self):
         return 
-    
+
+
+class ScaledPolytrope(Polytrope):
+    def __init__(self, gamma, polytropic_index, z0, z, **args):
+        Polytrope.__init__(self, gamma, polytropic_index, z0, z, **args)
+        self.scale_factor = (self.z0-self.z)
+                
+    def grad_ln_rho(self):
+        return -self.polytropic_index*np.ones_like(self.z)
+
+    def grad_S(self):
+        entropy_gradient_factor = ((self.polytropic_index+1)/self.gamma - self.polytropic_index)
+        print(entropy_gradient_factor)
+        print(self.epsilon/self.gamma)
+        return -entropy_gradient_factor*np.ones_like(self.z)
+
+    def grad_ln_T(self):
+        return -np.ones_like(self.z)
+
+    def rho(self):
+        return (self.z0-self.z)**(self.polytropic_index-1)
+
+    def T(self):
+        return np.ones_like(self.z)
+
+    def P(self):
+        return (self.z0-self.z)**(self.polytropic_index)
+
+    def S(self):
+        return 
 
 
 
 if __name__ == "__main__":
 
     Lz = 10
-    nz = 512*3/2
+    Lz = 740
+    nz = 1024*3/2
     
     z_basis = d2.Chebyshev(nz, interval=[0., Lz], dealias=2/3)
 
@@ -150,4 +188,4 @@ if __name__ == "__main__":
     
     num_coeffs = 20
 
-    atm = Polytrope(gamma, poly_n, Lz+1, z_basis, num_coeffs=num_coeffs)
+    atm = ScaledPolytrope(gamma, poly_n, Lz+1, z_basis, num_coeffs=num_coeffs)
