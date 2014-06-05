71da49369bed6dd8805084d9593c534e83ccdcd6
Keaton J. Burns <keaton.burns@gmail.com>
2014-06-05 15:30:32 -0400

    Join real and complex FFTW FFT classes into a single FourierTransform class.  Update numpy pointer references from .data attribute to PyArray_DATA() interface.
    hg hash: 9a2798b18e3383cd23d65f6d16c7c7f688133add

--- dedalus2/libraries/fftw/fftw_wrappers.pyx
+++ dedalus2/libraries/fftw/fftw_wrappers.pyx
@@ -183,35 +183,53 @@ cdef class Transpose:
                                    <double *> data.data)
 
 
-cdef class ComplexFourierTransform:
+cdef class FourierTransform:
     """
-    FFTW-based complex-to-complex discrete Fourier transform along one
-    axis of an N-dimensional array.
+    FFTW-based discrete Fourier transform along one axis of an N-dimensional array.
 
     Parameters
     ----------
+    grid_dtype : dtype
+        Grid space data type
     grid_shape : tuple of ints, array of ints
         Array shape in grid space
     axis : int
-        Axis over which to compute the DFT
+        Axis over which to compute the FFT
     flags : list of str, optional
-        List of wrapped FFTW flags (default: ['FFTW_MEASURE'])
+        List of wrapped FFTW flags
 
     """
 
-    cdef int grid_size
+    cdef readonly forward
+    cdef readonly backward
     cdef cfftw.fftw_plan forward_plan
     cdef cfftw.fftw_plan backward_plan
 
-    def __init__(self, grid_shape, int axis, flags=['FFTW_MEASURE']):
+    def __init__(self, grid_dtype, grid_shape, int axis, flags=[]):
 
+        # Dispatch transform methods
+        if grid_dtype == np.float64:
+            self.forward = self._forward_real
+            self.backward = self._backward_real
+        elif grid_dtype == np.complex128:
+            self.forward = self._forward_complex
+            self.backward = self._backward_complex
+        else:
+            raise ValueError("Unsupported dtype: %s" %str(grid_dtype))
+
+        # Compute coeff shape based on dtype
+        if grid_dtype == np.float64:
+            coeff_shape = list(grid_shape)
+            coeff_shape[axis] = grid_shape[axis]//2 + 1
+        elif grid_dtype == np.complex128:
+            coeff_shape = list(grid_shape)
         gshape = np.array(grid_shape, dtype=int)
-        cshape = np.array(grid_shape, dtype=int)
-        self.grid_size = gshape[axis]
+        cshape = np.array(coeff_shape, dtype=int)
 
-        # Build FFTW guru transform structures
+        # Build FFTW guru transform structure
         cdef int trans_rank = 1
         cdef cfftw.fftw_iodim trans
+        # Transform along `axis`
         trans.n = gshape[axis]
         trans.in_stride  = np.prod(gshape[axis+1:])
         trans.out_stride = np.prod(cshape[axis+1:])
@@ -220,9 +238,11 @@ cdef class ComplexFourierTransform:
         # Build FFTW guru vector structures
         cdef int vec_rank = 2
         cdef cfftw.fftw_iodim vec0, vec1f, vec1b
+        # Loop over higher axes
         vec0.n = np.prod(gshape[axis+1:])
         vec0.in_stride = 1
         vec0.out_stride = 1
+        # Loop over lower axes
         vec1f.n = vec1b.n = np.prod(gshape[:axis])
         vec1f.in_stride  = vec1b.out_stride = np.prod(gshape[axis:])
         vec1f.out_stride = vec1b.in_stride  = np.prod(cshape[axis:])
@@ -234,131 +254,49 @@ cdef class ComplexFourierTransform:
         for f in flags:
             intflags = intflags | fftw_flags[f]
 
-        # Create plans using temporary memory allocations
-        cdef complex *gdata
-        cdef complex *cdata
-        gdata = cfftw.fftw_alloc_complex(np.prod(gshape))
-        cdata = cfftw.fftw_alloc_complex(np.prod(cshape))
-        self.forward_plan = cfftw.fftw_plan_guru_dft(trans_rank,
-                                                     trans_struct,
-                                                     vec_rank,
-                                                     vec_struct_f,
-                                                     gdata,
-                                                     cdata,
-                                                     cfftw.FFTW_FORWARD,
-                                                     intflags | cfftw.FFTW_DESTROY_INPUT)
-        self.backward_plan = cfftw.fftw_plan_guru_dft(trans_rank,
-                                                      trans_struct,
-                                                      vec_rank,
-                                                      vec_struct_b,
-                                                      cdata,
-                                                      gdata,
-                                                      cfftw.FFTW_BACKWARD,
-                                                      intflags | cfftw.FFTW_DESTROY_INPUT)
-        cfftw.fftw_free(gdata)
-        cfftw.fftw_free(cdata)
-
-        # Check that plan creation succeeded
-        if (self.forward_plan == NULL) or (self.backward_plan == NULL):
-            raise RuntimeError("FFTW could not create plans.")
-
-    def __dealloc__(self):
-        """Destroy plans on deallocation."""
-
-        cfftw.fftw_destroy_plan(self.forward_plan)
-        cfftw.fftw_destroy_plan(self.backward_plan)
-
-    def forward(self, cnp.ndarray gdata, cnp.ndarray cdata):
-        """Grid-to-coefficient transform."""
-
-        # Execute plan using new-array interface
-        cfftw.fftw_execute_dft(self.forward_plan,
-                               <complex *> gdata.data,
-                               <complex *> cdata.data)
-        cdata /= self.grid_size
-
-    def backward(self, cnp.ndarray cdata, cnp.ndarray gdata):
-        """Coefficient-to-grid transform."""
-
-        # Execute plan using new-array interface
-        cfftw.fftw_execute_dft(self.backward_plan,
-                               <complex *> cdata.data,
-                               <complex *> gdata.data)
-
-
-cdef class RealFourierTransform:
-    """
-    FFTW-based real-to-complex discrete Fourier transform along one
-    axis of an N-dimensional array.
-
-    Parameters
-    ----------
-    grid_shape : tuple of ints, array of ints
-        Array shape in grid space
-    axis : int
-        Axis over which to compute the DFT
-    flags : list of str, optional
-        List of wrapped FFTW flags (default: ['FFTW_MEASURE'])
-
-    """
-
-    cdef int grid_size
-    cdef cfftw.fftw_plan forward_plan
-    cdef cfftw.fftw_plan backward_plan
-
-    def __init__(self, grid_shape, int axis, flags=['FFTW_MEASURE']):
-
-        gshape = np.array(grid_shape, dtype=int)
-        cshape = np.array(grid_shape, dtype=int)
-        cshape[axis] = gshape[axis] // 2 + 1
-        self.grid_size = gshape[axis]
-
-        # Build FFTW guru transform structures
-        cdef int trans_rank = 1
-        cdef cfftw.fftw_iodim trans
-        trans.n = gshape[axis]
-        trans.in_stride  = np.prod(gshape[axis+1:])
-        trans.out_stride = np.prod(cshape[axis+1:])
-        cdef cfftw.fftw_iodim *trans_struct = [trans]
-
-        # Build FFTW guru vector structures
-        cdef int vec_rank = 2
-        cdef cfftw.fftw_iodim vec0, vec1f, vec1b
-        vec0.n = np.prod(gshape[axis+1:])
-        vec0.in_stride = 1
-        vec0.out_stride = 1
-        vec1f.n = vec1b.n = np.prod(gshape[:axis])
-        vec1f.in_stride  = vec1b.out_stride = np.prod(gshape[axis:])
-        vec1f.out_stride = vec1b.in_stride  = np.prod(cshape[axis:])
-        cdef cfftw.fftw_iodim *vec_struct_f = [vec0, vec1f]
-        cdef cfftw.fftw_iodim *vec_struct_b = [vec0, vec1b]
-
-        # Build flags
-        cdef unsigned intflags = 0
-        for f in flags:
-            intflags = intflags | fftw_flags[f]
-
-        # Create plans using temporary memory allocations
-        cdef double *gdata
-        cdef complex *cdata
-        gdata = cfftw.fftw_alloc_real(np.prod(gshape))
-        cdata = cfftw.fftw_alloc_complex(np.prod(cshape))
-        self.forward_plan = cfftw.fftw_plan_guru_dft_r2c(trans_rank,
+        # Create out-of-place plans using temporary memory allocations
+        cdef double *rg_temp
+        cdef complex *cg_temp
+        cdef complex *cc_temp
+        if grid_dtype == np.float64:
+            rg_temp = cfftw.fftw_alloc_real(np.prod(gshape))
+            cc_temp = cfftw.fftw_alloc_complex(np.prod(cshape))
+            self.forward_plan = cfftw.fftw_plan_guru_dft_r2c(trans_rank,
+                                                             trans_struct,
+                                                             vec_rank,
+                                                             vec_struct_f,
+                                                             rg_temp,
+                                                             cc_temp,
+                                                             intflags | cfftw.FFTW_DESTROY_INPUT)
+            self.backward_plan = cfftw.fftw_plan_guru_dft_c2r(trans_rank,
+                                                              trans_struct,
+                                                              vec_rank,
+                                                              vec_struct_b,
+                                                              cc_temp,
+                                                              rg_temp,
+                                                              intflags | cfftw.FFTW_DESTROY_INPUT)
+        elif grid_dtype == np.complex128:
+            cg_temp = cfftw.fftw_alloc_complex(np.prod(gshape))
+            cc_temp = cfftw.fftw_alloc_complex(np.prod(cshape))
+            self.forward_plan = cfftw.fftw_plan_guru_dft(trans_rank,
                                                          trans_struct,
                                                          vec_rank,
                                                          vec_struct_f,
-                                                         gdata,
-                                                         cdata,
+                                                         cg_temp,
+                                                         cc_temp,
+                                                         cfftw.FFTW_FORWARD,
                                                          intflags | cfftw.FFTW_DESTROY_INPUT)
-        self.backward_plan = cfftw.fftw_plan_guru_dft_c2r(trans_rank,
+            self.backward_plan = cfftw.fftw_plan_guru_dft(trans_rank,
                                                           trans_struct,
                                                           vec_rank,
                                                           vec_struct_b,
-                                                          cdata,
-                                                          gdata,
+                                                          cc_temp,
+                                                          cg_temp,
+                                                          cfftw.FFTW_BACKWARD,
                                                           intflags | cfftw.FFTW_DESTROY_INPUT)
-        cfftw.fftw_free(gdata)
-        cfftw.fftw_free(cdata)
+        cfftw.fftw_free(rg_temp)
+        cfftw.fftw_free(cg_temp)
+        cfftw.fftw_free(cc_temp)
 
         # Check that plan creation succeeded
         if (self.forward_plan == NULL) or (self.backward_plan == NULL):
@@ -370,22 +308,37 @@ cdef class RealFourierTransform:
         cfftw.fftw_destroy_plan(self.forward_plan)
         cfftw.fftw_destroy_plan(self.backward_plan)
 
-    def forward(self, cnp.ndarray gdata, cnp.ndarray cdata):
-        """Grid-to-coefficient transform."""
+    def _forward_real(self, cnp.ndarray gdata, cnp.ndarray cdata):
+        """Real grid-to-coefficient transform."""
 
         # Execute plan using new-array interface
         cfftw.fftw_execute_dft_r2c(self.forward_plan,
-                                   <double *> gdata.data,
-                                   <complex *> cdata.data)
-        cdata /= self.grid_size
+                                   <double *> cnp.PyArray_DATA(gdata),
+                                   <complex *> cnp.PyArray_DATA(cdata))
 
-    def backward(self, cnp.ndarray cdata, cnp.ndarray gdata):
-        """Coefficient-to-grid transform."""
+    def _forward_complex(self, cnp.ndarray gdata, cnp.ndarray cdata):
+        """Complex grid-to-coefficient transform."""
+
+        # Execute plan using new-array interface
+        cfftw.fftw_execute_dft(self.forward_plan,
+                               <complex *> cnp.PyArray_DATA(gdata),
+                               <complex *> cnp.PyArray_DATA(cdata))
+
+    def _backward_real(self, cnp.ndarray cdata, cnp.ndarray gdata):
+        """Real coefficient-to-grid transform."""
 
         # Execute plan using new-array interface
         cfftw.fftw_execute_dft_c2r(self.backward_plan,
-                                   <complex *> cdata.data,
-                                   <double *> gdata.data)
+                                   <complex *> cnp.PyArray_DATA(cdata),
+                                   <double *> cnp.PyArray_DATA(gdata))
+
+    def _backward_complex(self, cnp.ndarray cdata, cnp.ndarray gdata):
+        """Complex coefficient-to-grid transform."""
+
+        # Execute plan using new-array interface
+        cfftw.fftw_execute_dft(self.backward_plan,
+                               <complex *> cnp.PyArray_DATA(cdata),
+                               <complex *> cnp.PyArray_DATA(gdata))
 
 
 cdef class DiscreteCosineTransform:
@@ -401,7 +354,7 @@ cdef class DiscreteCosineTransform:
     axis : int
         Axis over which to compute the DFT
     flags : list of str, optional
-        List of wrapped FFTW flags (default: ['FFTW_MEASURE'])
+        List of wrapped FFTW flags
 
     """
 
@@ -410,14 +363,14 @@ cdef class DiscreteCosineTransform:
 
     def __init__(self, grid_dtype, grid_shape, int axis, flags=[]):
 
-        # Check dtype
+        # Compute data shapes based on dtype
         if np.dtype(grid_dtype) == np.float64:
             real_shape = list(grid_shape)
         elif np.dtype(grid_dtype) == np.complex128:
             # Treat as interleaved real data
             real_shape = list(grid_shape) + [2]
         else:
-            raise ValueError("Invalid dtype: %s" %str(grid_dtype))
+            raise ValueError("Unsupported dtype: %s" %str(grid_dtype))
         gshape = np.array(real_shape, dtype=int)
         cshape = np.array(real_shape, dtype=int)
 
@@ -453,7 +406,7 @@ cdef class DiscreteCosineTransform:
         cdef int *kind_f = [cfftw.FFTW_REDFT10]
         cdef int *kind_b = [cfftw.FFTW_REDFT01]
 
-        # Create plans using temporary memory allocations
+        # Create out-of-place plans using temporary memory allocations
         cdef double *gdata
         cdef double *cdata
         gdata = cfftw.fftw_alloc_real(np.prod(gshape))
@@ -492,14 +445,14 @@ cdef class DiscreteCosineTransform:
 
         # Execute plan using new-array interface
         cfftw.fftw_execute_r2r(self.forward_plan,
-                               <double *> gdata.data,
-                               <double *> cdata.data)
+                               <double *> cnp.PyArray_DATA(gdata),
+                               <double *> cnp.PyArray_DATA(cdata))
 
     def backward(self, cnp.ndarray cdata, cnp.ndarray gdata):
         """Coefficient-to-grid transform."""
 
         # Execute plan using new-array interface
         cfftw.fftw_execute_r2r(self.backward_plan,
-                               <double *> cdata.data,
-                               <double *> gdata.data)
+                               <double *> cnp.PyArray_DATA(cdata),
+                               <double *> cnp.PyArray_DATA(gdata))
 
