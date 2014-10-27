f7fe443bc6e235a31c9b51b9033a98656e2b9b8e
Keaton J. Burns <keaton.burns@gmail.com>
2014-10-27 15:03:08 -0400

    Reformulate Future class to include basic operator structure.  Introduce subclasses of Future for different output types of operations (Scalar/Array/Field).
    hg hash: f0a2b4c230e4c746a636a01b9795343af2ab648e

--- dedalus2/data/future.py
+++ dedalus2/data/future.py
@@ -5,66 +5,220 @@ Classes for future evaluation.
 
 from functools import partial
 
+from .field import Operand, Data, Scalar, Array, Field
+from .metadata import Metadata
 
-class Future:
+
+class Future(Operand):
     """
-    An object whose value is updated as the simulation progresses.
+    Base class for deferred operations on data.
+
+    Parameters
+    ----------
+    *args : Operands
+        Operands. Number must match class attribute `arity`, if present.
+    out : data, optional
+        Output data object.  If not specified, a new object will be used.
+
+    Notes
+    -----
+    Operators are stacked (i.e. provided as arguments to other operators) to
+    construct trees that represent compound expressions.  Nodes are evaluated
+    by first recursively evaluating their subtrees, and then calling the
+    `operate` method.
 
     """
 
+    arity = None
     __array_priority__ = 100.
+    store_last = False
 
-    # def __getattr__(self, attr):
-    #     # Intercept numpy ufunc calls
-    #     if attr in operators.UfuncWrapper.supported:
-    #         ufunc = operators.UfuncWrapper.supported[attr]
-    #         return partial(operators.UfuncWrapper, ufunc, self)
-    #     else:
-    #         raise AttributeError("%r object has no attribute %r" %(self.__class__.__name__, attr))
+    def __init__(self, *args, domain=None, out=None):
 
-    def __abs__(self):
-        return operators.Absolute(self)
+        # Check arity
+        if self.arity is not None:
+            if len(args) != self.arity:
+                raise ValueError("Wrong number of arguments.")
+        # Infer domain from arguments
+        if domain is None:
+            domain = unique_domain(out, *args)
+        # Required attributes
+        self.args = list(args)
+        self.original_args = list(args)
+        self.domain = domain
+        self.out = out
+        self.last_id = None
 
-    def __neg__(self):
-        return operators.Negate(self)
+    def reset(self):
+        """Restore original arguments."""
+        self.args = list(self.original_args)
 
-    def __add__(self, other):
-        return operators.Add(self, other)
+    def __repr__(self):
+        repr_args = map(repr, self.args)
+        return '{}({})'.format(self.name, ', '.join(repr_args))
 
-    def __radd__(self, other):
-        # Addition is commutative
-        return operators.Add(self, other)
+    def __str__(self):
+        str_args = map(str, self.args)
+        return '{}({})'.format(self.name, ', '.join(str_args))
 
-    def __sub__(self, other):
-        # self - other
-        return self + (-1)*other
-        #return operators.Subtract(self, other)
+    def atoms(self, *types, include_out=True):
+        """"""
+        atoms = set()
+        # Recursively collect atoms
+        for arg in self.args:
+            if isinstance(arg, (Data, Future)):
+                atoms.update(arg.atoms(*types, include_out=include_out))
+        # Include output as directed
+        if include_out:
+            if isinstance(self.out, types):
+                atoms.add(self.out)
 
-    def __rsub__(self, other):
-        # other - self
-        return other + (-1)*self
-        #return operators.Subtract(other, self)
+        return atoms
 
-    def __mul__(self, other):
-        return operators.Multiply(self, other)
+    def has(self, *atoms):
+        return any((a in self.atoms() for a in atoms))
 
-    def __rmul__(self, other):
-        # Multiplication is commutative
-        return operators.Multiply(self, other)
+    def evaluate(self, id=None, force=True):
+        """Recursively evaluate operation."""
 
-    def __truediv__(self, other):
-        # Input: self / other
-        return self * other**(-1)
-        #return operators.Divide(self, other)
+        # Check storage
+        if self.store_last and (id is not None):
+            if id == self.last_id:
+                return self.last_out
+            else:
+                # Clear cache to free output field
+                self.last_id = None
+                self.last_out = None
 
-    def __rtruediv__(self, other):
-        # Input: other / self
-        return other * self**(-1)
-        #return operators.Divide(other, self)
+        # Recursively attempt evaluation of all operator arguments
+        # Track evaluation success with flag
+        all_eval = True
+        for i, a in enumerate(self.args):
+            if isinstance(a, Field):
+                a.set_scales(self.domain.dealias, keep_data=True)
+            if isinstance(a, Future):
+                a_eval = a.evaluate(id=id, force=force)
+                # If evaluation succeeds, substitute result
+                if a_eval is not None:
+                    self.args[i] = a_eval
+                # Otherwise change flag
+                else:
+                    all_eval = False
 
-    def __pow__(self, other):
-        return operators.Power(self, other)
+        # Return None if any arguments are not evaluable
+        if not all_eval:
+            return None
+
+        # Check conditions unless forcing evaluation
+        if not force:
+            # Return None if operator conditions are not satisfied
+            if not self.check_conditions():
+                return None
+
+        # Allocate output field if necessary
+        if self.out:
+            out = self.out
+        else:
+            out = self.domain.new_data(self.future_type)
+
+        # Copy metadata
+        out.meta = self.meta
+        out.set_scales(self.domain.dealias, keep_data=False)
+
+        # Perform operation
+        self.operate(out)
+
+        # Reset to free temporary field arguments
+        self.reset()
+
+        # Update storage
+        if self.store_last and (id is not None):
+            self.last_id = id
+            self.last_out = out
+
+        return out
+
+    def attempt(self, id=None):
+        """Recursively attempt to evaluate operation."""
+        return self.evaluate(id=id, force=False)
+
+    @property
+    def meta(self):
+        meta = Metadata(self.domain)
+        for axis in range(self.domain.dim):
+            for key in meta[axis]:
+                meta[axis][key] = getattr(self, 'meta_%s' %key)(axis)
+        return meta
+
+    def meta_scale(self, axis):
+        # Perform all operations at the dealias scale
+        return self.domain.bases[axis].dealias
+
+    def check_conditions(self):
+        """Check that all argument fields are in proper layouts."""
+        # This method must be implemented in derived classes and should return
+        # a boolean indicating whether the operation can be computed without
+        # changing the layout of any of the field arguments.
+        raise NotImplementedError()
+
+    def operate(self, out):
+        """Perform operation."""
+        # This method must be implemented in derived classes, take an output
+        # field as its only argument, and evaluate the operation into this
+        # field without modifying the data of the arguments.
+        raise NotImplementedError()
 
 
-from . import operators
+class FutureScalar(Future):
+    """Class for deferred operations producing a Scalar."""
+    future_type = Scalar
+    meta = Scalar.ScalarMeta()
+
+    def __init__(self, *args, domain=None, out=None):
+
+        # Check arity
+        if self.arity is not None:
+            if len(args) != self.arity:
+                raise ValueError("Wrong number of arguments.")
+        # Required attributes
+        self.args = list(args)
+        self.original_args = list(args)
+        self.domain = domain
+        self.out = out
+        self.last_id = None
+
+
+class FutureArray(Future):
+    """Class for deferred operations producing an Array."""
+    future_type = Array
+
+
+class FutureField(Future):
+    """Class for deferred operations producing a Field."""
+    future_type = Field
+
+    @staticmethod
+    def parse(string, namespace, domain):
+        """Build FutureField from a string expression."""
+        expression = eval(string, namespace)
+        return FutureField.cast(expression, domain)
+
+    @staticmethod
+    def cast(input, domain):
+        """Cast an object to a FutureField."""
+        from .operators import FieldCopy
+        # Cast to operand
+        input = Operand.cast(input, domain=domain)
+        # Cast to FutureField
+        if isinstance(input, FutureField):
+            return input
+        else:
+            return FieldCopy(input, domain)
+
+
+def unique_domain(*args):
+    """Return unique domain from a set of fields."""
+    from .domain import combine_domains
+    domains = [getattr(arg, 'domain', None) for arg in args]
+    return combine_domains(*domains)
 
