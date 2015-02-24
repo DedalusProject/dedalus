d9e5158996c876bfc30f7db496df834724c19aea
Keaton J. Burns <keaton.burns@gmail.com>
2015-02-23 19:21:57 -0500

    Update 2d poisson example
    hg hash: 8a87e8ba4497ba8555aad12bfaf8044429bb6823

--- examples/bvp/2d_poisson/poisson.py
+++ examples/bvp/2d_poisson/poisson.py
@@ -11,6 +11,7 @@ On a single process, this should take just a few seconds to run.
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 from dedalus import public as de
 from dedalus.extras import plot_tools
@@ -22,7 +23,7 @@ y_basis = de.Chebyshev('y', 128, interval=(0, 1))
 domain = de.Domain([x_basis, y_basis], grid_dtype=np.float64)
 
 # 2D Boussinesq hydrodynamics
-problem = de.BVP(domain, variables=['u','uy'])
+problem = de.LBVP(domain, variables=['u','uy'])
 problem.add_equation("dx(dx(u)) + dy(uy) = -10 * sin(x/2)**2 * (y - y**2)")
 problem.add_equation("uy - dy(u) = 0")
 problem.add_bc("left(u) = left(sin(8*x))")
@@ -35,5 +36,5 @@ solver.solve()
 # Plot solution
 u = solver.state['u']
 u.require_grid_space()
-fig = plot_tools.plot_bot_2d(u)
-fig.savefig('poisson.png')
+plot_tools.plot_bot_2d(u)
+plt.savefig('poisson.png')
