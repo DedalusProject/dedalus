b5c734e37b6112b2e7941f7ffdac579b829116ae
Keaton J. Burns <keaton.burns@gmail.com>
2015-02-25 14:18:43 -0500

    Docstring typo
    hg hash: 9b4355223d81dad9badacc817969742f2eb6dcfa

--- examples/ivp/3d_rayleigh_benard/rayleigh_benard.py
+++ examples/ivp/3d_rayleigh_benard/rayleigh_benard.py
@@ -14,7 +14,7 @@ can be used to plot the slices.
 To run, merge, and plot using 4 processes, for instance, you could use:
     $ mpiexec -n 4 python3 rayleigh_benard.py
     $ mpiexec -n 4 python3 merge.py snapshots
-    $ mpiexec -n 4 python3 plot_2d_series.py plot snapshots/*.h5
+    $ mpiexec -n 4 python3 plot_2d_series.py snapshots/*.h5
 
 The simulation should take roughly 400 process-minutes to run, but will
 automatically stop after an hour.
