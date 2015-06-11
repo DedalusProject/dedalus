006ba46b66dec5ee9ff4419ef682dc45ffaa43c4
Keaton J. Burns <keaton.burns@gmail.com>
2015-06-11 12:02:35 -0400

    Clean up subsystems, working towards excluding non-computable modes
    hg hash: bb28ae33270f010e9e2877bdec3d5dac44170026

--- dedalus/core/subsystems.py
+++ dedalus/core/subsystems.py
@@ -55,92 +55,125 @@ def build_matrices(subsystems, problem, matrices):
 
 class Subsystem:
     """
-    Object holding problem matrices for a given transverse wavevector.
+    Object representing one coupled subsystem of a problem.
 
-    Parameters
-    ----------
-    index : tuple of ints
-        Transverse indeces for retrieving pencil from system data
+    Subsystems are identified by their group multi-index, which identifies
+    the corresponding subgroup of each separable dimension of the problem.
 
+    This is the generalization of the pencils in a problem with exactly one
+    coupled dimension.
     """
 
     def __init__(self, subdomain, group):
         self.subdomain = subdomain
         self.domain = subdomain.domain
-        self.local_start = self.domain.dist.coeff_layout.start(subdomain, scales=1)
         self.group = group
+        local_start = group - self.domain.dist.coeff_layout.start(subdomain, scales=1)
+        self.local_group = group - local_start
 
-    def global_slices(self, bases):
-        slices = []
+    @CachedAttribute
+    def group_dict(self):
+        """Group dictionary for evaluation equation conditions."""
+        group_dict = {}
+        for axis, group in enumerate(self.group):
+            if group is not None:
+                for space in self.domain.spaces[axis]:
+                    group_dict['n'+space.name] = group
+        return group_dict
+
+    def group_shape(self, bases):
+        """Computable-coefficient shape for group."""
+        group_shape = []
         for group, basis in zip(self.group, bases):
             if basis is None:
                 if group in [0, None]:
-                    slices.append(slice(0, 1))
+                    group_shape.append(1)
                 else:
-                    slices.append(slice(1, 1))
+                    group_shape.append(0)
             else:
                 if group is None:
-                    slices.append(slice(None))
+                    group_shape.append(basis.n_modes)
                 else:
-                    start = basis.space.group_size * group
-                    end = basis.space.group_size * (group + 1)
-                    slices.append(slice(start, end))
-        return slices
+                    group_shape.append(basis.group_size(group))
+        return tuple(group_shape)
+
+    def group_size(self, bases):
+        """Number of computable coefficients for group."""
+        group_shape = self.group_shape(bases)
+        return np.prod(group_shape)
+
+    def global_start(self, bases):
+        """Global starting index of group."""
+        global_start = []
+        for group, basis in zip(self.group, bases):
+            if basis is None:
+                if group in [0, None]:
+                    global_start.append(0)
+                else:
+                    global_start.append(1)
+            else:
+                if group is None:
+                    global_start.append(0)
+                else:
+                    global_start.append(group * basis.space.group_spacing)
+        return tuple(global_start)
+
+    def local_start(self, bases):
+        """Local starting index of group."""
+        local_start = []
+        for group, basis in zip(self.local_group, bases):
+            if basis is None:
+                if group in [0, None]:
+                    local_start.append(0)
+                else:
+                    local_start.append(1)
+            else:
+                if group is None:
+                    local_start.append(0)
+                else:
+                    local_start.append(group * basis.space.group_spacing)
+        return local_start
+
+    def global_slices(self, bases):
+        """Global slices for computable coefficients."""
+        global_start = self.global_start(bases)
+        group_shape = self.group_shape(bases)
+        global_slices = []
+        for start, shape in zip(global_start, group_shape):
+            global_slices.append(slice(start, start+shape))
+        return tuple(global_slices)
 
     def local_slices(self, bases):
-        slices = []
-        for group, basis, start in zip(self.group, bases, self.local_start):
-            if bases is None:
-                if group in [0, None]:
-                    slices.append(slice(0, 1))
-                else:
-                    slices.append(slice(1, 1))
-            else:
-                if group is None:
-                    slices.append(slice(None))
-                else:
-                    start = basis.space.group_size * group - start
-                    end = basis.space.group_size * (group + 1) - start
-                    slices.append(slice(start, end))
-        return slices
+        """Global slices for computable coefficients."""
+        local_start = self.local_start(bases)
+        group_shape = self.group_shape(bases)
+        local_slices = []
+        for start, shape in zip(local_start, group_shape):
+            local_slices.append(slice(start, start+shape))
+        return tuple(local_slices)
 
     def get_vector(self, vars):
+        """Retrieve and concatenate group data from variables."""
         vec = []
         for var in vars:
-            slices = self.local_slices(var.bases)
-            var_data = var['c'][slices]
-            if var_data.size:
+            if self.group_size(var):
+                slices = self.local_slices(var.bases)
+                var_data = var['c'][slices]
                 vec.append(var_data.ravel())
         return np.concatenate(vec)
 
-    def set_vector(self, vars, X):
+    def set_vector(self, vars, data):
+        """Assign vectorized group data to variables."""
         i0 = 0
         for var in vars:
-            slices = self.local_slices(var.bases)
-            var_data = var['c'][slices].ravel()
-            if var_data.size:
-                i1 = i0 + var_data.size
-                np.copyto(var_data.ravel(), X[i0:i1])
+            group_size = self.group_size(var):
+            if group_size:
+                i1 = i0 + group_size
+                slices = self.local_slices(var.bases)
+                var_data = var['c'][slices]
+                vec_data = data[i0:i1].reshape(var_data.shape)
+                np.copyto(var_data, vec_data)
                 i0 = i1
-
-    def shape(self, bases):
-        shape = []
-        for group, basis in zip(self.group, bases):
-            if basis is None:
-                if group in [0, None]:
-                    shape.append(1)
-                else:
-                    shape.append(0)
-            else:
-                if group is None:
-                    shape.append(basis.space.coeff_size)
-                else:
-                    shape.append(basis.space.group_size)
-        return shape
-
-    def size(self, bases):
-        shape = self.shape(bases)
-        return np.prod(shape)
 
     def compute_identities(self, bases):
         cid = []
@@ -160,59 +193,52 @@ class Subsystem:
         return reduce(sparse.kron, axmats, 1).tocsr()
 
     def build_matrices(self, problem, names):
-
-        # Build group dictionary for evaluating equation conditions
-        group_dict = {}
-        for axis, group in enumerate(self.group):
-            if group is not None:
-                for space in self.domain.spaces[axis]:
-                    group_dict['n'+space.name] = group
+        """Build problem matrices."""
 
         # Filter equations by condition and group
-        eqs = [eq for eq in problem.eqs if eval(eq['raw_condition'], group_dict)]
+        eqs = [eq for eq in problem.eqs if eval(eq['raw_condition'], self.group_dict)]
         eqs = [eq for eq in eqs if self.size(eq['bases'])]
+        eq_sizes = [self.size(eq['bases']) for eq in eqs]
 
         # Filter variables by group
         vars = [var for var in problem.variables if self.size(var.bases)]
+        var_sizes = [self.size(var.bases) for var in vars]
 
-        # Build subsystem matrices
-        I = sum(self.size(eq['bases']) for eq in eqs)
-        J = sum(self.size(var.bases) for var in vars)
-        matrices = {name: sparse.lil_matrix((I, J)) for name in names}
+        # Require squareness
+        I = sum(eq_sizes)
+        J = sum(var_sizes)
+        if I != J:
+            raise ValueError("Non-square system.")
 
-        # Add subsystem entries
-        for name, matrix in matrices.items():
-            rows = []
-            cols = []
-            data = []
-
-            i0 = i1 = 0
-            for eq in eqs:
+        # Construct subsystem matrices
+        matrices = {}
+        for name in names:
+            # Collect subblocks
+            data, rows, cols = [], [], []
+            i0 = 0
+            for eq, eq_size in zip(eqs, eq_sizes):
                 expr = eq[name][0]
                 if expr != 0:
                     op_dict = expr.operator_dict(self, vars, **problem.ncc_kw)
                     j0 = 0
-                    for var in vars:
+                    for var, var_size in zip(vars, var_sizes):
                         if var in op_dict:
                             varmat = op_dict[var].tocoo()
+                            data.append(varmat.data)
                             rows.append(varmat.row + i0)
                             cols.append(varmat.col + j0)
-                            data.append(varmat.data)
-                        j0 += self.size(var.bases)
-                i0 += self.size(eq['bases'])
-
+                        j0 += var_size
+                i0 += eq_size
+            # Build full matrix
+            data = np.concatenate(data)
             rows = np.concatenate(rows)
             cols = np.concatenate(cols)
-            data = np.concatenate(data)
             matrices[name] = sparse.coo_matrix((data, (rows, cols)), shape=(I,J)).tocsr()
 
         # Store minimal CSR matrices for fast dot products
         for name, matrix in matrices.items():
             matrix.eliminate_zeros()
             setattr(self, name, matrix.tocsr())
-
-        F_conv = [self.compute_conversion(eq['F'].bases, eq['bases']) for eq in eqs]
-        self.RHS_C = sparse.block_diag(F_conv).tocsr()
 
         # Store expanded CSR matrices for fast combination
         self.LHS = zeros_with_pattern(*matrices.values()).tocsr()
@@ -221,5 +247,6 @@ class Subsystem:
             setattr(self, name+'_exp', matrix.tocsr())
 
         # Store RHS conversion matrix
-
+        F_conv = [self.compute_conversion(eq['F'].bases, eq['bases']) for eq in eqs]
+        self.RHS_C = sparse.block_diag(F_conv).tocsr()
 
