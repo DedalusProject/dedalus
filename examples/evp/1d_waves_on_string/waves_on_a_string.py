d5d6b217754ffa75b1251775147a69522bd0f28c
Keaton J. Burns <keaton.burns@gmail.com>
2017-08-17 21:24:01 -0400

    Update EVP examples with recent features, more comments, etc.
    hg hash: f3fc89a6eb8adaae2ad8d456e449fbea8590a338

--- examples/evp/1d_waves_on_string/waves_on_a_string.py
+++ examples/evp/1d_waves_on_string/waves_on_a_string.py
@@ -1,47 +1,61 @@
-"""Dedalus script to compute eigenvalues and vectors for waves on a
-string on a Chebyshev basis.
+"""
+Dedalus script to compute eigenmodes for waves on a string in a Chebyshev basis.
 
-We solve
-
-l * u + dx(dx(u)) = 0
-
+We solve:
+    l*u + dx(dx(u)) = 0
 where l is the eigenvalue. Careful! Don't use "lambda": this is a
 python reserved word!
 
 This is a somewhat challenging problem because the eigenvectors are
 sines and cosines, but we expand them in Chebyshev polynomials.
 
-reference: J. P. Boyd, "Chebyshev and Fourier Spectral Methods", 2nd
-Edition (2001).
+Reference:
+    J. P. Boyd, "Chebyshev and Fourier Spectral Methods", 2nd edition (2001), sec 7.2.
 
 """
+
 import dedalus.public as de
 import numpy as np
 import matplotlib.pyplot as plt
-
 import logging
 logger = logging.getLogger(__name__)
 
-# Build domain
-x_basis = de.Chebyshev('x', 128, interval=(-1, 1))
+
+# Domain
+Nx = 128
+x_basis = de.Chebyshev('x', Nx, interval=(-1, 1))
 domain = de.Domain([x_basis], np.float64)
 
+# Problem
 problem = de.EVP(domain, variables=['u', 'u_x'],eigenvalue='l')
+problem.meta[:]['x']['dirichlet'] = True
 problem.add_equation("l*u + dx(u_x) = 0")
 problem.add_equation("u_x - dx(u) = 0")
 problem.add_bc("left(u) = 0")
 problem.add_bc("right(u) = 0")
 
+# Solver
 solver = problem.build_solver()
-
 solver.solve(solver.pencils[0])
 
-evalue_number = 1 + np.arange(len(solver.eigenvalues))
-analytic_solution = np.pi**2/4. * (evalue_number)**2 
+# Filter infinite/nan eigenmodes
+finite = np.isfinite(solver.eigenvalues)
+solver.eigenvalues = solver.eigenvalues[finite]
+solver.eigenvectors = solver.eigenvectors[:, finite]
 
-solver.eigenvalues.sort()
+# Sort eigenmodes by eigenvalue
+order = np.argsort(solver.eigenvalues)
+solver.eigenvalues = solver.eigenvalues[order]
+solver.eigenvectors = solver.eigenvectors[:, order]
 
-plt.semilogy(evalue_number,(np.abs(solver.eigenvalues - analytic_solution))/analytic_solution)
-plt.xlabel("eigenvalue number")
-plt.ylabel(r"$|\lambda - \lambda_{analytic}|/\lambda_{analytic}$")
+# Plot error vs exact eigenvalues
+mode_number = 1 + np.arange(len(solver.eigenvalues))
+exact_eigenvalues = mode_number**2 * np.pi**2 / 4
+eval_relative_error = (solver.eigenvalues - exact_eigenvalues) / exact_eigenvalues
+
+plt.figure()
+plt.semilogy(mode_number, np.abs(eval_relative_error))
+plt.xlabel("Mode number")
+plt.ylabel(r"$|\lambda - \lambda_{exact}|/\lambda_{exact}$")
+plt.title(r"Eigenvalue relative error ($N_x=%i$)" %Nx)
 plt.savefig('eval_error.png')
