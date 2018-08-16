0e18885a944a788d5e5ead3c359db93a992063f9
Keaton J. Burns <keaton.burns@gmail.com>
2018-08-16 11:19:43 -0400

    Remove Hg inspection from setup.py and requirements
    hg hash: fab1faea323f116a6be9e8bc7e464363bda7bd95

--- setup.py
+++ setup.py
@@ -12,31 +12,19 @@ import glob
 
 FFTW_STATIC='FFTW_STATIC'
 
-# Look for Mercurial tools
-try:
-    import hgapi
-except ImportError:
-    hgapi = None
-
 def check_env_var(env_var):
     path = None
     if env_var in os.environ:
         path = os.environ[env_var]
         print("  Found env var %s = %s" %(env_var, path))
-
     return path
 
 def get_prefix(name):
     """
-    Get prefix path for libraries containing string <name>.
-
-    First checks environment variables for <NAME>_PATH, then searches a few
-    other likely places.
-
+    Get prefix path for libraries containing string <name>. First checks environment
+    variables for <NAME>_PATH, then searches a few other likely places.
     """
-
     print("Looking for %s prefix" %name)
-
     # Check for environment variable
     patterns = ['%s_PATH',
                 '%s_PREFIX']
@@ -45,7 +33,6 @@ def get_prefix(name):
         path = check_env_var(env_var)
         if path:
             return path
-
     # Check likely places
     places = ['/usr/lib/openmpi', # MPI under Ubuntu
               '/usr',
@@ -58,7 +45,6 @@ def get_prefix(name):
         if matches:
             print("  Found matching library in %s" %placelib)
             return place
-
     print("  Cannot find env var %s_PATH or libraries matching %s." %(name.upper(), name))
     print("  If %s isn't in your LD_LIBRARY_PATH, compilation will likely fail." %name)
 
@@ -67,7 +53,6 @@ def get_include(name):
     path = check_env_var(env_var)
     if path:
         return path
-
     prefix = get_prefix(name)
     return os.path.join(prefix, 'include')
 
@@ -76,36 +61,8 @@ def get_lib(name):
     path = check_env_var(env_var)
     if path:
         return path
-
     prefix = get_prefix(name)
     return os.path.join(prefix, 'lib')
-
-def hg_info(path):
-    # Resolve symoblic paths
-    path = pathlib.Path(path).resolve()
-    # Get mercurial repository
-    repo = hgapi.Repo(str(path))
-    return repo.hg_id(), repo.hg_diff()
-
-# Modify build_ext to record hg repository info, if possible
-if hgapi:
-    try:
-        class custom_build_ext(build_ext):
-            def run(self):
-                # honor the --dry-run flag
-                if not self.dry_run:
-                    target_dir = os.path.join(self.build_lib,'dedalus')
-                    src_dir =  os.getcwd()
-                    id, diff = hg_info(src_dir)
-                    self.mkpath(target_dir)
-                    with open(os.path.join(target_dir, '__hg_version__.py'), 'w') as fobj:
-                        fobj.write("hg_version = '%s'\n" %id)
-                        fobj.write("diff = %s\n" %diff)
-                    build_ext.run(self)
-    except hgapi.HgException:
-        custom_build_ext = build_ext
-else:
-    custom_build_ext = build_ext
 
 include_dirs = ['dedalus/libraries/fftw/',
                 np.get_include(),
@@ -174,6 +131,5 @@ setup(
                 'dedalus.core',
                 'dedalus.libraries',
                 'dedalus.libraries.fftw'],
-    cmdclass = {'build_ext': custom_build_ext},
     package_data = {'dedalus': ['dedalus.cfg']},
     ext_modules = cythonize(extensions))
