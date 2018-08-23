036cf2823510026f954960519c5ecb627257cb2b
Keaton J. Burns <keaton.burns@gmail.com>
2018-08-23 15:37:44 -0400

    Implement pyproject.toml and other modifications for modern build with pip
    hg hash: 1885c7f6c33d6e07c98498e0426abadcb77128d8

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ from setuptools import setup
 from setuptools import Extension
 from Cython.Build import cythonize
 from distutils.util import strtobool
-import pathlib
 import numpy as np
 import mpi4py
 import os
 import glob
 
-FFTW_STATIC='FFTW_STATIC'
 
+# Helper functions for finding C-dependency paths
 def check_env_var(env_var):
     path = None
     if env_var in os.environ:
@@ -64,36 +63,35 @@ def get_lib(name):
     prefix = get_prefix(name)
     return os.path.join(prefix, 'lib')
 
+# C-dependency paths for extension compilation and linking
 include_dirs = ['dedalus/libraries/fftw/',
                 np.get_include(),
                 mpi4py.get_include(),
                 get_include('fftw'),
                 get_include('mpi')]
-
-libraries = ['fftw3_mpi',
-             'fftw3',
-             'm']
-
+libraries = ['fftw3_mpi', 'fftw3', 'm']
 library_dirs = [get_lib('fftw')]
 
+# Optionally set static linking for FFTW
+FFTW_STATIC='FFTW_STATIC'
 fs = os.getenv(FFTW_STATIC)
 if fs:
     fftw_static=bool(strtobool(fs))
 else:
     fftw_static=False
-
 if fftw_static:
     print("Statically linking FFTW to allow Dedalus to work with Intel MKL.")
     fftw_lib_path = get_lib('fftw')
     extra_link_args = ["-Xlinker",
-                         "-Bsymbolic",
-                         "-Wl,--whole-archive",
-                         "{}/libfftw3.a".format(fftw_lib_path),
-                         "{}/libfftw3_mpi.a".format(fftw_lib_path),
+                       "-Bsymbolic",
+                       "-Wl,--whole-archive",
+                       "{}/libfftw3.a".format(fftw_lib_path),
+                       "{}/libfftw3_mpi.a".format(fftw_lib_path),
                        "-Wl,--no-whole-archive"]
 else:
     extra_link_args = []
 
+# Extension objects for cython
 extensions = [
     Extension(
         name='dedalus.libraries.fftw.fftw_wrappers',
@@ -119,12 +117,31 @@ extensions = [
         library_dirs=library_dirs,
         extra_compile_args=["-Wno-error=declaration-after-statement"])]
 
+# Runtime requirements
+install_requires = [
+    "docopt",
+    "h5py >= 2.6.0",
+    "matplotlib",
+    "mpi4py >= 2.0.0",
+    "numpy",
+    "scipy >= 0.13.0"]
+
+# Grab long_description from README
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='dedalus',
-    version='2',
+    version='2.1808a2',
     author='Keaton J. Burns',
     author_email='keaton.burns@gmail.com',
+    description="A flexible framework for solving differential equations using spectral methods.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://dedalus-project.org',
+    classifiers=['Programming Language :: Python :: 3'],
+    install_requires=install_requires,
     license='GPL3',
-    packages = setuptools.find_packages(),
-    package_data = {'dedalus': ['dedalus.cfg']},
-    ext_modules = cythonize(extensions))
+    packages=setuptools.find_packages(),
+    package_data={'': ['dedalus.cfg']},
+    ext_modules=cythonize(extensions))
