e8b58f8c4a54cd90a4f3342052672a2aeddf70b9
Keaton J. Burns <keaton.burns@gmail.com>
2019-02-05 11:52:38 -0500

    coords work
    hg hash: 2ccd41f167a26589bb78258edec83f53051caae7

--- dedalus/core/coords.py
+++ dedalus/core/coords.py
@@ -1,66 +1,35 @@
 
 
-class Space:
-
-    def __init__(self, dim, name=None):
-        self.dim = dim
-        self.name = name
-
-    def __mul__(self, other):
-        return ProductSpace(self, other)
-
-
-class ProductSpace(Space):
-
-    def __init__(self, *spaces, name=None):
-        self.spaces = spaces
-        self.dim = sum(space.dim for space in spaces)
-        self.name = name
-
-
-
-
 class CoordinateSystem:
 
-    def __init__(self, space, names):
-        if space.dim != self.dim:
-            raise ValueError("Coordinate and space dimensions must match.")
+    def __init__(self, names, dist, axis):
         if len(names) != self.dim:
             raise ValueError("Number of coordinate names must match dimension.")
-        self.space = space
         self.names = names
+        self.dist = dist
+        self.axis = axis
 
 
 class Coordinate(CoordinateSystem):
-
     dim = 1
 
 
 class CartesianCoordinates(CoordinateSystem):
 
-    def __init__(self, space, names):
+    def __init__(self, names, dist, axis):
         self.dim = len(names)
-        super().__init__(space, names)
+        super().__init__(names, dist, axis)
 
 
 class S2Coordinates(CoordinateSystem):
-
     dim = 2
 
 
 class SphericalCoordinates(CoordinateSystem):
-
     dim = 3
 
 
 class PolarCoordinates(CoordinateSystem):
-
     dim = 2
-
-
-class CylindricalCoordinates(CoordinateSystem):
-
-    dim = 3
-
 
 
