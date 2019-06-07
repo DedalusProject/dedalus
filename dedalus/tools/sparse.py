1c60f4b38aa884e45de13fea32522c09ccc75211
J. S. Oishi <jsoishi@gmail.com>
2019-06-07 11:13:06 -0400

    Updated sparse.py for matsolve. This fixes issue #60.
    hg hash: a9da8dc63c93f00ba930571ea0020bcf5cc23981

--- dedalus/tools/sparse.py
+++ dedalus/tools/sparse.py
@@ -7,10 +7,7 @@ from scipy import sparse
 from scipy.sparse import linalg as spla
 
 from .config import config
-STORE_LU = config['linear algebra'].getboolean('store_LU')
-PERMC_SPEC = config['linear algebra']['permc_spec']
-USE_UMFPACK = config['linear algebra'].getboolean('use_umfpack')
-
+from ..libraries.matsolvers import matsolvers
 
 def scipy_sparse_eigs(A, B, N, target, **kw):
     """
@@ -32,15 +29,13 @@ def scipy_sparse_eigs(A, B, N, target, **kw):
 
     Other keyword options passed to scipy.sparse.linalg.eigs.
     """
+    matsolver = matsolvers[config['linear algebra']['MATRIX_SOLVER'].lower()]
+
     # Build sparse linear operator representing (A - σB)^I B = C^I B = D
     C = A - target * B
-    if STORE_LU:
-        C_LU = spla.splu(C.tocsc(), permc_spec=PERMC_SPEC)
-        def matvec(x):
-            return C_LU.solve(B.dot(x))
-    else:
-        def matvec(x):
-            return spla.spsolve(C, B.dot(x), use_umfpack=USE_UMFPACK, permc_spec=PERMC_SPEC)
+    solver = matsolver(C)
+    def matvec(x):
+        return solver.solve(B.dot(x))
     D = spla.LinearOperator(dtype=A.dtype, shape=A.shape, matvec=matvec)
     # Solve using scipy sparse algorithm
     evals, evecs = spla.eigs(D, k=N, which='LM', sigma=None, **kw)
