82dff159da65130419c8fbef092690e13519979d
Keaton J. Burns <keaton.burns@gmail.com>
2020-02-10 15:24:49 +1100

    Add new tests
    hg hash: 2b6fab21fe207215d16e8c914827993aa8fb3ae0

---
+++ dedalus/tests/test_new.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+from dedalus.core import coords, distributor, basis, field
+
+c = coords.CartesianCoordinates('x', 'y')
+d = distributor.Distributor(c.coords)
+xb = basis.ComplexFourier(c.coords[0], size=8, bounds=(1,2))
+yb = basis.Jacobi(c.coords[1], size=8, a=0, b=0, a0=0, b0=0, bounds=(3,4))
+u = field.Field(dist=d, bases=(xb,yb), dtype=np.complex128)
+
+u.set_scales(0.5)
+u['c'][1, 1] = 1
+uc0 = u['c'].copy()
+u['g']
+print(np.allclose(u['c'], uc0))
+
+
