Fixed gradient. Test problem wasn't actually testing the gradient of the vector!
hg hash: ac72e4e6bf47babc9f9ab06025f9e6fdbbcda6ec

dedalus/core/basis.py
@@ -1294,23 +1294,26 @@ class BallBasis(RegularityBasis):
         regsig = regorder[np.array(regindex)]
         return regsig.sum()
 
-    def n_limits(self, regindex, ell):
+    def n_limits(self, regindex, ell, Nmax=None):
+        if Nmax == None: Nmax = self.Nmax
         if not self.regularity_allowed(ell, regindex):
             return None
         regtotal = self.regtotal(regindex)
         nmin = dedalus_sphere.ball.Nmin(ell, regtotal)
-        return (nmin, self.Nmax)
+        return (nmin, Nmax)
 
-    def n_size(self, regindex, ell):
+    def n_size(self, regindex, ell, Nmax=None):
+        if Nmax == None: Nmax = self.Nmax
         if not self.regularity_allowed(ell, regindex):
             return 0
-        nmin, nmax = self.n_limits(regindex, ell)
+        nmin, nmax = self.n_limits(regindex, ell, Nmax=Nmax)
         return nmax - nmin + 1
 
-    def n_slice(self, regindex, ell):
+    def n_slice(self, regindex, ell, Nmax=None):
+        if Nmax == None: Nmax = self.Nmax
         if not self.regularity_allowed(ell, regindex):
             return None
-        nmin, nmax = self.n_limits(regindex, ell)
+        nmin, nmax = self.n_limits(regindex, ell, Nmax=Nmax)
         return slice(nmin, nmax+1)
 
 

dedalus/tests/test_new.py
@@ -189,7 +189,7 @@ Tg0[1,1] = 6*x**2/(r**2*np.sin(theta)**2) - (6*x**2+4*y*z)/r**2
 Tg0[1,0] = Tg0[0,1] = -2*x*(x**2+y**2+3*y*z)/(r**3*np.sin(theta)**2)
 Tg0[0,0] = 6*y**2/(x**2+y**2)
 
-result = np.allclose(u['g'], ug)
+result = np.allclose(T['g'], Tg0)
 results.append(result)
 print(len(results), ':', result)
 