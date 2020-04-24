fd81dd1f3f27a49fa0ea20d7105a614b926cd275
Keaton J. Burns <keaton.burns@gmail.com>
2020-04-24 13:16:48 -0400

    Clean up RBC example to use strings
    hg hash: 2364a51e457ccfd386f8b3f7c448b2e421455cf7

--- dedalus/tests/test_new_rbc.py
+++ dedalus/tests/test_new_rbc.py
@@ -3,6 +3,7 @@
 import numpy as np
 from dedalus.core import coords, distributor, basis, field, operators, problems, solvers, timesteppers
 from dedalus.tools import logging
+from dedalus.tools.parsing import split_equation
 
 import logging
 logger = logging.getLogger(__name__)
@@ -26,35 +27,37 @@ p = field.Field(name='p', dist=d, bases=(xb,yb,zb), dtype=np.complex128)
 b = field.Field(name='b', dist=d, bases=(xb,yb,zb), dtype=np.complex128)
 u = field.Field(name='u',dist=d, bases=(xb,yb,zb), dtype=np.complex128, tensorsig=(c,))
 
-# Equations [M, L, F]
+# Parameters and operators
 P = (Rayleigh * Prandtl)**(-1/2)
 R = (Rayleigh / Prandtl)**(-1/2)
 ez = field.Field(name='ez', dist=d, bases=(xb,yb,zb), dtype=np.complex128, tensorsig=(c,))
 ez['g'][2] = 1
 ghat = - ez
-div = operators.Divergence
-lap = operators.Laplacian
-grad = operators.Gradient
-dot = operators.DotProduct
-dt = operators.TimeDerivative
+div = lambda A: operators.Divergence(A, index=0)
+lap = lambda A: operators.Laplacian(A, c)
+grad = lambda A: operators.Gradient(A, c)
+dot = lambda A, B: operators.DotProduct(A, B)
+dt = lambda A: operators.TimeDerivative(A)
 
+# Problem
+def eq_eval(eq_str):
+    return [eval(expr) for expr in split_equation(eq_str)]
 problem = problems.IVP([p, b, u])
-problem.add_equation([div(u,0), 0])
-problem.add_equation([dt(b) - P*lap(b,c), -dot(u,grad(b,c))])
-problem.add_equation([dt(u) - R*lap(u,c) + grad(p,c), -dot(u,grad(u,c)) - b*ghat])
-problem.add_equation([u(z=0), 0])
-problem.add_equation([u(z=Lz), 0])
-problem.add_equation([b(z=0), Lz])
-problem.add_equation([b(z=Lz), 0])
-# Pressure gauge?
-
+problem.add_equation(eq_eval("div(u) = 0"))
+problem.add_equation(eq_eval("dt(b) - P*lap(b) = - dot(u,grad(b))"))
+problem.add_equation(eq_eval("dt(u) - R*lap(u) + grad(p) = - dot(u,grad(u)) - b*ghat"))
+problem.add_equation(eq_eval("u(z=0) = 0"))
+problem.add_equation(eq_eval("u(z=Lz) = 0"))
+problem.add_equation(eq_eval("b(z=0) = Lz"))
+problem.add_equation(eq_eval("b(z=Lz) = 0"))
+# pressure gauge?
 print("Problem built")
 
+# Solver
 solver = solvers.InitialValueSolver(problem, timesteppers.RK111)
 
-
+# Plot matrices
 import matplotlib.pyplot as plt
-
 plt.figure()
 I = 2
 J = 2
