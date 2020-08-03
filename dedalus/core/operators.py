Fixed k != 0 transforms. Added in a hack to make gradients work: we're currently off by a factor of sqrt(2).

dedalus/core/operators.py
@@ -2160,9 +2160,11 @@ class PolarGradient(Gradient, PolarMOperator):
     @CachedMethod
     def _radial_matrix(radial_basis, spinindex_out0, spintotal, m):
         if spinindex_out0 == 0:
-            return radial_basis.operator_matrix('D-', m, spintotal)
+            # HACK: added in a 1/sqrt(2) factor just to make things work
+            return 1/np.sqrt(2)*radial_basis.operator_matrix('D-', m, spintotal)
         elif spinindex_out0 == 1:
-            return radial_basis.operator_matrix('D+', m, spintotal)
+            # HACK: added ina a 1/sqrt(2) factor just to make things work
+            return 1/np.sqrt(2)*radial_basis.operator_matrix('D+', m, spintotal)
         else:
             raise ValueError("This should never happen")
 

dedalus/core/transforms.py
@@ -1058,9 +1058,9 @@ class DiskRadialTransform(NonSeparableTransform):
             if m not in m_matrices:
                 Nmin = dedalus_sphere.zernike.min_degree(m)
                 Nc = self.N2c - Nmin
-                W = dedalus_sphere.zernike.polynomials(2, Nc, self.k + self.alpha, np.abs(m + self.s), z_grid) # shape (N2c-Nmin, Ng)
+                W = dedalus_sphere.zernike.polynomials(2, Nc, self.alpha, np.abs(m + self.s), z_grid) # shape (N2c-Nmin, Ng)
                 conversion = dedalus_sphere.zernike.operator(2, 'E')(+1)**self.k
-                W = conversion(Nc, self.k + self.alpha, np.abs(m + self.s)) @ W
+                W = conversion(Nc, self.alpha, np.abs(m + self.s)) @ W
                 W = (W*weights).astype(np.float64)
                 # zero out modes higher than grid resolution taking into account n starts at Nmin
                 W[self.N2g-Nmin:] = 0