0bfb6aa1bff8226ed5107e9b1ff19c979a437463
Keaton J. Burns <keaton.burns@gmail.com>
2020-10-01 14:04:49 -0400

    Remove arithmetic cython file

--- setup.py
+++ setup.py
@@ -161,15 +161,6 @@ extensions = [
         library_dirs=library_dirs,
         runtime_library_dirs=library_dirs,
         extra_compile_args=extra_compile_args,
-        extra_link_args=extra_link_args),
-    Extension(
-        name='dedalus.libraries.arithmetic',
-        sources=['dedalus/libraries/arithmetic.pyx'],
-        include_dirs=include_dirs,
-        libraries=libraries,
-        library_dirs=library_dirs,
-        runtime_library_dirs=library_dirs,
-        extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args)]#,
 #    Extension(
 #        name='dedalus.core.polynomials',
