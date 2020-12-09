40112cc14773f52e5069c5595c36194380770d1a
Benjamin Brown <bpbrown@gmail.com>
2020-12-09 10:29:49 -0800

    Begins test of diffusion EVPs in the ball, includes analytic eigenvalue script.

--- dedalus/tests/test_evp.py
+++ dedalus/tests/test_evp.py
@@ -7,6 +7,7 @@ import numpy as np
 import scipy.special as spec
 import functools
 from dedalus.core import coords, distributor, basis, field, operators, problems, solvers, timesteppers, arithmetic
+from ball_diffusion_analytical_eigenvalues import eigenvalues as analytic_eigenvalues
 
 
 @pytest.mark.parametrize('dtype', [np.complex128])
@@ -113,3 +114,50 @@ def test_ball_bessel_eigenfunction(Lmax, Nmax, Leig, Neig, radius, dtype):
     sol = sol.ravel()
     sol /= sol[i_max]
     assert np.allclose(radial_eigenfunction, sol)
+
+
+@pytest.mark.parametrize('dtype', [np.complex128])
+@pytest.mark.parametrize('Lmax', [3])
+@pytest.mark.parametrize('Nmax', [31])
+@pytest.mark.parametrize('Leig', [1,2,3])
+@pytest.mark.parametrize('radius', [1,1.5])
+@pytest.mark.parametrize('bc', ['no-slip', 'stress-free'])
+def test_ball_diffusion(Lmax, Nmax, Leig, radius, bc, dtype):
+    # Bases
+    c = coords.SphericalCoordinates('phi', 'theta', 'r')
+    d = distributor.Distributor((c,))
+    b = basis.BallBasis(c, (2*(Lmax+1), Lmax+1, Nmax+1), radius=radius, dtype=dtype)
+    b_S2 = b.S2_basis()
+    phi, theta, r = b.local_grids((1, 1, 1))
+    # Fields
+    A = field.Field(dist=d, bases=(b,), tensorsig=(c,), dtype=dtype)
+    φ = field.Field(dist=d, bases=(b,), dtype=dtype)
+    τ_A = field.Field(dist=d, bases=(b_S2,), tensorsig=(c,), dtype=dtype)
+    λ = field.Field(name='λ', dist=d, dtype=dtype)
+    # Parameters and operators
+    div = lambda A: operators.Divergence(A)
+    grad = lambda A: operators.Gradient(A, c)
+    lap = lambda A: operators.Laplacian(A, c)
+    trans = lambda A: operators.TransposeComponents(A)
+    radial = lambda A, index: operators.RadialComponent(A, index=index)
+    angular = lambda A, index: operators.AngularComponent(A, index=index)
+    LiftTau = lambda A: operators.LiftTau(A, b, -1)
+
+    problem = problems.EVP([φ,A,τ_A], λ)
+    problem.add_equation((div(A), 0))
+    problem.add_equation((-λ*A + grad(φ) - lap(A) + LiftTau(τ_A), 0))
+    if bc == 'no-slip':
+        problem.add_equation((A(r=radius), 0))
+    elif bc == 'stress-free':
+        E = 1/2*(grad(A) + trans(grad(A)))
+        problem.add_equation((radial(A(r=radius),0), 0))
+        problem.add_equation((radial(angular(E(r=radius),0),1), 0))
+    # Solver
+    solver = solvers.EigenvalueSolver(problem)
+    if not solver.subproblems[Leig].group[1] == Leig:
+        raise ValueError("subproblems indexed in a strange way")
+    solver.solve_dense(solver.subproblems[Leig])
+    i_sort = np.argsort(solver.eigenvalues)
+    solver.eigenvalues = solver.eigenvalues[i_sort]
+    λ_analytic = analytic_eigenvalues(Leig,Nmax,bc, r0=radius)
+    assert np.allclose(solver.eigenvalues[:Nmax//4], λ_analytic[:Nmax//4])
