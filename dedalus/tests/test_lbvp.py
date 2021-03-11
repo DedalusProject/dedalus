40a9f3617a53382ab46e91a452b460ecba649d16
Keaton J. Burns <keaton.burns@gmail.com>
2021-03-11 09:36:56 -0500

    Fix up new sphere ncc tests to be radius independent

--- dedalus/tests/test_lbvp.py
+++ dedalus/tests/test_lbvp.py
@@ -183,45 +183,6 @@ def test_ncc_ball(Nmax, Lmax, dtype):
 @pytest.mark.parametrize('dtype', [np.complex128, np.float64])
 @pytest.mark.parametrize('Nmax', [15])
 @pytest.mark.parametrize('Lmax', [3])
-@pytest.mark.parametrize('ncc_exponent', [0]) #,1,2,3/2])
-@pytest.mark.parametrize('ncc_location', ['LHS', 'RHS'])
-@pytest.mark.parametrize('ncc_scale', [1, 3/2])
-def test_heat_ncc_ball(Nmax, Lmax, ncc_exponent, ncc_location, ncc_scale, dtype):
-    # Bases
-    dealias = 1
-    c, d, b, phi, theta, r, x, y, z = build_ball(2*(Lmax+1), Lmax+1, Nmax+1, dealias=dealias, dtype=dtype)
-    # Fields
-    u = field.Field(name='u', dist=d, bases=(b,), dtype=dtype)
-    τu = field.Field(name='u', dist=d, bases=(b.S2_basis(),), dtype=dtype)
-    ncc = field.Field(name='ncc', dist=d, bases=(b.radial_basis,), dtype=dtype)
-    F = field.Field(name='a', dist=d, bases=(b,), dtype=dtype)
-    # Test Parameters
-    F_value  = {0:6, 1:2, 2:1, 3/2:3/4}
-    analytic = {0:r**2-1, 1:r-1, 2:np.log(r), 3/2:np.sqrt(r)-1}
-    if ncc_location=='RHS':
-        ncc['g'] = 1
-        F['g'] = F_value[ncc_exponent]*r**(-ncc_exponent)
-    else:
-        ncc['g'] = r**ncc_exponent
-        F['g'] = F_value[ncc_exponent]
-    u_true = analytic[ncc_exponent]
-    ncc.require_scales(ncc_scale)
-    ncc['g'] # force transform
-    # Problem
-    Lap = lambda A: operators.Laplacian(A, c)
-    LiftTau = lambda A: operators.LiftTau(A, b, -1)
-    problem = problems.LBVP([u, τu])
-    problem.add_equation((ncc*Lap(u) + LiftTau(τu), F))
-    problem.add_equation((u(r=radius_ball), 0))
-    # Solver
-    solver = solvers.LinearBoundaryValueSolver(problem)
-    solver.solve()
-    # Check solution
-    assert np.allclose(u['g'], u_true)
-
-@pytest.mark.parametrize('dtype', [np.complex128, np.float64])
-@pytest.mark.parametrize('Nmax', [15])
-@pytest.mark.parametrize('Lmax', [3])
 @pytest.mark.parametrize('ncc_exponent', [0,1,2,3/2])
 @pytest.mark.parametrize('ncc_location', ['LHS', 'RHS'])
 @pytest.mark.parametrize('ncc_scale', [1, 3/2])
@@ -261,39 +222,25 @@ def test_heat_ncc_shell(Nmax, Lmax, ncc_exponent, ncc_location, ncc_scale, dtype
     # Check solution
     assert np.allclose(u['g'], u_true)
 
-radius_pi_ball = np.sqrt(np.pi/2)
-@CachedFunction
-def build_pi_ball(Nphi, Ntheta, Nr, dealias, dtype):
-    c = coords.SphericalCoordinates('phi', 'theta', 'r')
-    d = distributor.Distributor((c,))
-    b = basis.BallBasis(c, (Nphi, Ntheta, Nr), radius=radius_pi_ball, dealias=(dealias, dealias, dealias), dtype=dtype)
-    phi, theta, r = b.local_grids()
-    x, y, z = c.cartesian(phi, theta, r)
-    return c, d, b, phi, theta, r, x, y, z
-
 @pytest.mark.parametrize('dtype', [np.complex128, np.float64])
 @pytest.mark.parametrize('Nmax', [15])
 @pytest.mark.parametrize('Lmax', [3])
-@pytest.mark.parametrize('ncc_test', ['u'])# '1/g'])
 @pytest.mark.parametrize('ncc_scale', [1, 3/2])
-def test_heat_ncc_cos_ball(Nmax, Lmax, ncc_test, ncc_scale, dtype):
+def test_heat_ncc_cos_ball(Nmax, Lmax, ncc_scale, dtype):
     # Bases
     dealias = 1
-    c, d, b, phi, theta, r, x, y, z = build_pi_ball(2*(Lmax+1), Lmax+1, Nmax+1, dealias=dealias, dtype=dtype)
+    c, d, b, phi, theta, r, x, y, z = build_ball(2*(Lmax+1), Lmax+1, Nmax+1, dealias=dealias, dtype=dtype)
     # Fields
     u = field.Field(name='u', dist=d, bases=(b,), dtype=dtype)
     τu = field.Field(name='u', dist=d, bases=(b.S2_basis(),), dtype=dtype)
     ncc = field.Field(name='ncc', dist=d, bases=(b.radial_basis,), dtype=dtype)
     F = field.Field(name='a', dist=d, bases=(b,), dtype=dtype)
     # Test Parameters
-    u_true = np.cos(r**2)
-    g = - 4*r**2*np.cos(r**2) -6*np.sin(r**2)
-    if ncc_test=='u':
-        ncc['g'] = u_true
-        F['g'] = u_true*g
-    else:
-        ncc['g'] = 1/g
-        F['g'] = 1
+    R = radius_ball
+    u_true = np.cos(np.pi/2*(r/R)**2)
+    g = - np.pi/2/R**2 * (4*np.pi/2*(r/R)**2*np.cos(np.pi/2*(r/R)**2) + 6*np.sin(np.pi/2*(r/R)**2))
+    ncc['g'] = u_true
+    F['g'] = u_true*g
     ncc.require_scales(ncc_scale)
     ncc['g'] # force transform
     # Problem
@@ -301,7 +248,7 @@ def test_heat_ncc_cos_ball(Nmax, Lmax, ncc_test, ncc_scale, dtype):
     LiftTau = lambda A: operators.LiftTau(A, b, -1)
     problem = problems.LBVP([u, τu])
     problem.add_equation((ncc*Lap(u) + LiftTau(τu), F))
-    problem.add_equation((u(r=radius_pi_ball), 0))
+    problem.add_equation((u(r=radius_ball), 0))
     # Solver
     solver = solvers.LinearBoundaryValueSolver(problem)
     solver.solve()
