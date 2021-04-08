5089283edfe42fd1b91303f6eab0b4988170a6c2
Benjamin Brown <bpbrown@gmail.com>
2021-04-08 11:24:07 -0700

    Generalizes test_ivp to run over all timesteppers.  Adds ChebyshevT test and ncc test.

--- dedalus/tests/test_ivp.py
+++ dedalus/tests/test_ivp.py
@@ -24,13 +24,83 @@ def test_heat_1d_periodic(x_basis_class, Nx, timestepper, dtype):
     F['g'] = -np.sin(x)
     # Problem
     dx = lambda A: operators.Differentiate(A, c)
-    dt = operators.TimeDerivative
+    ddt = operators.TimeDerivative
     problem = problems.IVP([u])
-    problem.add_equation((-dt(u) + dx(dx(u)), F))
+    problem.add_equation((-ddt(u) + dx(dx(u)), F))
     # Solver
     solver = solvers.InitialValueSolver(problem, timestepper)
     dt = 1e-5
-    iter = 10
+    iter = 100
+    for i in range(iter):
+        solver.step(dt)
+    # Check solution
+    amp = 1 - np.exp(-solver.sim_time)
+    u_true = amp * np.sin(x)
+    assert np.allclose(u['g'], u_true)
+
+@pytest.mark.parametrize('dtype', [np.complex128, np.float64])
+@pytest.mark.parametrize('timestepper', timesteppers.schemes)
+@pytest.mark.parametrize('Nx', [32])
+@pytest.mark.parametrize('x_basis_class', [basis.ChebyshevT])
+def test_heat_1d_periodic_chebyshev(x_basis_class, Nx, timestepper, dtype):
+    # Bases
+    c = coords.Coordinate('x')
+    d = distributor.Distributor((c,))
+    xb = x_basis_class(c, size=Nx, bounds=(0, 2*np.pi))
+    x = xb.local_grid(1)
+    # Fields
+    u = field.Field(name='u', dist=d, bases=(xb,), dtype=dtype)
+    F = field.Field(name='F', dist=d, bases=(xb,), dtype=dtype)
+    F['g'] = -np.sin(x)
+    τu = field.Field(name='τu', dist=d, dtype=dtype)
+    P = field.Field(name='P1', dist=d, bases=(xb,), dtype=dtype)
+    P['c'][-1] = 1
+    # Problem
+    dx = lambda A: operators.Differentiate(A, c)
+    ddt = operators.TimeDerivative
+    problem = problems.IVP([u,τu])
+    problem.add_equation((-ddt(u) + P*τu + dx(dx(u)), F))
+    problem.add_equation((u(x=0)-u(x=2*np.pi),0))
+    # Solver
+    solver = solvers.InitialValueSolver(problem, timesteppers.schemes[timestepper])
+    dt = 1e-5
+    iter = 100
+    for i in range(iter):
+        solver.step(dt)
+    # Check solution
+    amp = 1 - np.exp(-solver.sim_time)
+    u_true = amp * np.sin(x)
+    assert np.allclose(u['g'], u_true)
+
+@pytest.mark.parametrize('dtype', [np.complex128, np.float64])
+@pytest.mark.parametrize('timestepper', timesteppers.schemes)
+@pytest.mark.parametrize('Nx', [32])
+@pytest.mark.parametrize('dt', [1e-5,1e-6,1e-7])
+@pytest.mark.parametrize('x_basis_class', [basis.ChebyshevT])
+def test_heat_1d_periodic_ncc(x_basis_class, Nx, timestepper, dt, dtype):
+    # Bases
+    c = coords.Coordinate('x')
+    d = distributor.Distributor((c,))
+    xb = x_basis_class(c, size=Nx, bounds=(0, 2*np.pi))
+    x = xb.local_grid(1)
+    # Fields
+    u = field.Field(name='u', dist=d, bases=(xb,), dtype=dtype)
+    F = field.Field(name='F', dist=d, bases=(xb,), dtype=dtype)
+    F['g'] = -np.sin(x)
+    ncc = field.Field(name='ncc', dist=d, bases=(xb,), dtype=dtype)
+    ncc['g'] = np.cos(4*x)+2
+    τu = field.Field(name='τu', dist=d, dtype=dtype)
+    P = field.Field(name='P1', dist=d, bases=(xb,), dtype=dtype)
+    P['c'][-1] = 1
+    # Problem
+    dx = lambda A: operators.Differentiate(A, c)
+    ddt = operators.TimeDerivative
+    problem = problems.IVP([u,τu])
+    problem.add_equation((-ncc*ddt(u) + P*τu + ncc*dx(dx(u)), ncc*F))
+    problem.add_equation((u(x=0)-u(x=2*np.pi),0))
+    # Solver
+    solver = solvers.InitialValueSolver(problem, timesteppers.schemes[timestepper])
+    iter = 100
     for i in range(iter):
         solver.step(dt)
     # Check solution
