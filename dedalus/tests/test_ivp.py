7da2d95085cbb3208ad54d18bec1701a115abce0
Benjamin Brown <bpbrown@gmail.com>
2021-04-08 15:05:53 -0700

    Adds ODE timestepper test, with and without ncc.

--- dedalus/tests/test_ivp.py
+++ dedalus/tests/test_ivp.py
@@ -188,3 +188,68 @@ def test_wave_1d_periodic_chebyshev_ncc(x_basis_class, Nx, k_ncc, timestepper, d
     # Check solution
     u_true = u0*np.exp(1j*k*solver.sim_time)
     assert np.allclose(u['g'], u_true)
+
+@pytest.mark.parametrize('dtype', [np.complex128])
+@pytest.mark.parametrize('dt', [1e-5])
+@pytest.mark.parametrize('timestepper', timesteppers.schemes)
+@pytest.mark.parametrize('Nx', [32])
+@pytest.mark.parametrize('x_basis_class', [basis.ChebyshevT])
+def test_1d_periodic_chebyshev(x_basis_class, Nx, timestepper, dt, dtype):
+    # Bases
+    c = coords.Coordinate('x')
+    d = distributor.Distributor((c,))
+    xb = x_basis_class(c, size=Nx, bounds=(0, 2*np.pi))
+    x = xb.local_grid(1)
+    # Fields
+    k = 4
+    u = field.Field(name='u', dist=d, bases=(xb,), dtype=dtype)
+    ut = field.Field(name='u', dist=d, bases=(xb,), dtype=dtype)
+    u['g'] = u0 = np.sin(k*x)
+    ut['g'] = 1j*k*u0
+    # Problem
+    ddt = operators.TimeDerivative
+    problem = problems.IVP([u])
+    problem.add_equation((ddt(u), ut))
+    # Solver
+    solver = solvers.InitialValueSolver(problem, timesteppers.schemes[timestepper])
+    iter = 100
+    for i in range(iter):
+        solver.step(dt)
+    # Check solution
+    u_true = u0*np.exp(1j*k*solver.sim_time)
+    assert np.allclose(u['g'], u_true)
+
+@pytest.mark.parametrize('dtype', [np.complex128])
+@pytest.mark.parametrize('dt', [1e-5])
+@pytest.mark.parametrize('timestepper', timesteppers.schemes)
+@pytest.mark.parametrize('k_ncc', [4])
+@pytest.mark.parametrize('Nx', [32])
+@pytest.mark.parametrize('x_basis_class', [basis.ChebyshevT])
+def test_1d_periodic_chebyshev_ncc(x_basis_class, Nx, k_ncc, timestepper, dt, dtype):
+    # Bases
+    c = coords.Coordinate('x')
+    d = distributor.Distributor((c,))
+    xb = x_basis_class(c, size=Nx, bounds=(0, 2*np.pi))
+    x = xb.local_grid(1)
+    # Fields
+    k = 4
+    u = field.Field(name='u', dist=d, bases=(xb,), dtype=dtype)
+    ut = field.Field(name='u', dist=d, bases=(xb,), dtype=dtype)
+    u['g'] = u0 = np.sin(k*x)
+    ut['g'] = 1j*k*u0
+    # Problem
+    ncc = field.Field(name='ncc', dist=d, bases=(xb,), dtype=dtype)
+    ncc['g'] = k_ncc
+    for ik in np.arange(1,k_ncc+1):
+        ncc['g'] += np.sqrt(ik/k_ncc)*np.cos(ik*x)
+    ddt = operators.TimeDerivative
+    problem = problems.IVP([u])
+    problem.add_equation((ncc*ddt(u), ncc*ut))
+    # Solver
+    solver = solvers.InitialValueSolver(problem, timesteppers.schemes[timestepper])
+    iter = 100
+    for i in range(iter):
+        solver.step(dt)
+    # Check solution
+    u_true = u0*np.exp(1j*k*solver.sim_time)
+    assert np.allclose(u['g'], u_true)
