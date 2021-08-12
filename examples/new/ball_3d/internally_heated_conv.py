2e392cb99d75b11cf78ca7fb5199114295a5fc4e
Daniel <lecoanet@Daniels-MacBook-Pro.local>
2021-08-12 00:39:10 -0400

    Internally heated convection with plotting scripts. The plotting scripts need a lot of work. The convection problem initially runs for 10 time units, but can be restarted to run to 20 time units.

---
+++ examples/new/ball_3d/internally_heated_conv.py
@@ -0,0 +1,167 @@
+"""
+
+This script simulates internally-heated convection. We solve the Boussinesq equations with
+a constant internal heating source term in the temperature equation.
+
+dt(u) + grad(p) - 1/R*laplacian(u) - r*T = -u.grad(u)
+div(u) = 0
+dt(T) - 1/P*laplacian(T) = -u.grad(T) + S/P
+
+where R = sqrt(Rayleigh*Prandtl) and P = sqrt(Rayleigh/Prandtl), and r is the radius vector.
+We use stress-free boundary conditions, and maintain the temperature on the outer boundary
+equal to 0. The convection is driven by the internal heating term S/P. The conductive
+equilibrium is T=S/6*(1-r^2).
+
+"""
+
+import numpy as np
+from dedalus.core import coords, distributor, basis, field, operators, problems, solvers, timesteppers, arithmetic
+from dedalus.tools import logging
+from dedalus.tools.parsing import split_equation
+from dedalus.extras.flow_tools import GlobalArrayReducer
+import time
+from mpi4py import MPI
+import sys
+
+import logging
+logger = logging.getLogger(__name__)
+
+from dedalus.tools.config import config
+config['linear algebra']['MATRIX_FACTORIZER'] = 'SuperLUNaturalFactorizedTranspose'
+
+
+restart = False
+if len(sys.argv) > 1 and sys.argv[1] == '--restart':
+    restart = True
+
+# Parameters
+Lmax = 63
+L_dealias = 3/2
+Nmax = 47
+N_dealias = 3/2
+if not restart:
+    t_end = 10.01
+else:
+    t_end = 20
+ts = timesteppers.SBDF2
+dtype = np.float64
+mesh = None#[4,8]
+Rayleigh = 1e6
+Prandtl = 1
+R = np.sqrt(Rayleigh*Prandtl)
+P = np.sqrt(Rayleigh/Prandtl)
+
+# Bases
+c = coords.SphericalCoordinates('phi', 'theta', 'r')
+d = distributor.Distributor((c,), mesh=mesh)
+b = basis.BallBasis(c, (2*(Lmax+1), Lmax+1, Nmax+1), radius=1, dtype=dtype)
+b_S2 = b.S2_basis()
+phi, theta, r = b.local_grids((1, 1, 1))
+x = r*np.sin(theta)*np.cos(phi)
+y = r*np.sin(theta)*np.sin(phi)
+z = r*np.cos(theta) + 0*phi
+
+# Fields
+u = field.Field(name='u', dist=d, bases=(b,), tensorsig=(c,), dtype=dtype)
+p = field.Field(name='p', dist=d, bases=(b,), dtype=dtype)
+T = field.Field(name='T', dist=d, bases=(b,), dtype=dtype)
+tau_u = field.Field(name='tau u', dist=d, bases=(b_S2,), tensorsig=(c,), dtype=dtype)
+tau_T = field.Field(name='tau T', dist=d, bases=(b_S2,), dtype=dtype)
+# TODO: basis for NCC
+r_vec = field.Field(dist=d, bases=(b.radial_basis,), tensorsig=(c,), dtype=dtype)
+r_vec['g'][2] = r
+T_source = field.Field(dist=d, bases=(b,), dtype=dtype)
+T_source['g'] = 6
+#T_source = 6
+
+# Operators
+div = operators.Divergence
+lap = lambda A: operators.Laplacian(A, c)
+grad = lambda A: operators.Gradient(A, c)
+curl = lambda A: operators.Curl(A)
+dot = arithmetic.DotProduct
+cross = arithmetic.CrossProduct
+ddt = operators.TimeDerivative
+LiftTau = lambda A: operators.LiftTau(A, b, -1)
+rad = operators.RadialComponent
+ang = operators.AngularComponent
+trans = operators.TransposeComponents
+grid = operators.Grid
+
+# Boundary conditions
+strain_rate = grad(u) + trans(grad(u))
+shear_stress = ang(rad(strain_rate(r=1)))
+
+# Problem
+def eq_eval(eq_str):
+    return [eval(expr) for expr in split_equation(eq_str)]
+problem = problems.IVP([u, p, T, tau_u, tau_T])
+problem.add_equation(eq_eval("ddt(u) - 1/R*lap(u) + grad(p) + LiftTau(tau_u) - r_vec*T = - cross(curl(u),u)"))
+problem.add_equation(eq_eval("div(u) = 0"))
+problem.add_equation(eq_eval("ddt(T) - 1/P*lap(T) + LiftTau(tau_T) = - dot(u,grad(T)) + grid(T_source)/P"))
+problem.add_equation(eq_eval("rad(u(r=1)) = 0"), condition="ntheta != 0")  # no penetration
+problem.add_equation(eq_eval("p(r=1) = 0"), condition="ntheta == 0")  # pressure gauge
+problem.add_equation(eq_eval("shear_stress = 0"))  # stress free
+problem.add_equation(eq_eval("T(r=1) = 0"))
+logger.info("Problem built")
+
+# Solver
+solver = solvers.InitialValueSolver(problem, ts)
+solver.stop_sim_time = t_end
+
+# Analysis
+# TODO: integral operators
+t_list = []
+E_list = []
+weight_theta = b.local_colatitude_weights(1)
+weight_r = b.local_radial_weights(1)
+reducer = GlobalArrayReducer(d.comm_cart)
+vol_test = np.sum(weight_r*weight_theta+0*p['g'])*np.pi/(Lmax+1)/L_dealias
+vol_test = reducer.reduce_scalar(vol_test, MPI.SUM)
+vol_correction = 4*np.pi/3/vol_test
+
+# Initial condition
+
+if not restart:
+    T['g'] = 1-r**2
+    #T['g'] += 0.04*r**3*(1-r**2)*(np.cos(3*phi)+np.sin(3*phi))*np.sin(theta)**3
+    #T['g'] += 0.3*np.exp(-((x+0.3)**2+y**2+z**2)/0.2**2)
+    T['g'] += 0.5*np.random.rand(*T['g'].shape)
+    mode = 'overwrite'
+else:
+    write, dt = solver.load_state('checkpoints/checkpoints_s11.h5')
+    mode = 'append'
+
+slices = solver.evaluator.add_file_handler('slices', sim_dt = 0.1, max_writes = 10, virtual_file=True, mode=mode)
+slices.add_task(T(theta=np.pi/2), name='T eq')
+slices.add_task(T(phi=0), name='T mer right')
+slices.add_task(T(phi=np.pi), name='T mer left')
+slices.add_task(T(r=1/2), name='T r=0.5')
+
+checkpoints = solver.evaluator.add_file_handler('checkpoints', sim_dt = 1, max_writes = 1, virtual_file=True, mode=mode)
+checkpoints.add_tasks(solver.state)
+
+# TODO: CFL
+dt = 0.5e-2
+
+hermitian_cadence = 100
+
+# Main loop
+start_time = time.time()
+while solver.ok:
+    if solver.iteration % 10 == 0:
+        E0 = np.sum(vol_correction*weight_r*weight_theta*u['g'].real**2)
+        E0 = 0.5*E0*(np.pi)/(Lmax+1)/L_dealias
+        E0 = reducer.reduce_scalar(E0, MPI.SUM)
+        logger.info("t = %f, E = %e" %(solver.sim_time, E0))
+        t_list.append(solver.sim_time)
+        E_list.append(E0)
+
+    # Impose hermitian symmetry on two consecutive timesteps because we are using a 2-stage timestepper
+    if solver.iteration % hermitian_cadence in [0, 1]:
+        for f in solver.state:
+            f.require_grid_space()
+
+    solver.step(dt)
+end_time = time.time()
+logger.info('Run time: %f' %(end_time-start_time))
---
+++ examples/new/ball_3d/plot_mer_series.py
@@ -0,0 +1,437 @@
+
+
+import h5py
+import sys
+import os
+import shutil
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from mpi4py import MPI
+import pathlib
+import multiprocessing
+
+from dedalus.tools import post
+from dedalus.tools.array import reshape_vector
+from dedalus.tools import general
+
+# Some settings
+nrows = 1
+ncols = 1
+
+cmap_name = 'RdBu_r'
+dpi = 300
+
+start = 0
+
+dir = sys.argv[1]
+
+start_num = 0
+
+saveroot = 'frames/'
+
+name = 'T_mer'
+
+savename = lambda index: '%s_%06i.png' %(name, index+start_num)
+
+data_min = 0
+data_max = 0
+
+def main(file, index, first, plot_list, timestring, mfig):
+
+    fnames = ['T mer right', 'T mer left']
+
+    for k, task in enumerate(fnames):
+        dset = file['tasks'][task]
+        write_num = file['scales/write_number'][index]
+        pcolormesh(mfig, k, task, dset, index, first, plot_list, write_num)
+
+    del dset
+
+    # Title
+    title = 't = %g' %file['scales/sim_time'][index]
+    if first:
+      title_height = 1 - 0.5 * mfig.margin.top / mfig.fig.y
+      timestring = mfig.figure.suptitle(title, y=title_height)
+    else:
+      timestring.set_text(title)
+
+    # Save
+    path = os.path.join(saveroot, savename(file['scales/write_number'][index]))
+    mfig.figure.savefig(path, dpi=dpi)
+#    plt.close(mfig.figure)
+
+    return timestring
+
+def pcolormesh(mfig, k, taskname, dset, index, first, plot_list, write_num):
+
+    # Pick data axes for x and y plot axes
+    # Note: we use the full time-space data array, so remember that axis 0 is time
+    xi, yi = (2, 3)
+
+    # Slices for data.
+    # First is time axis, here sliced by the "index" argument.
+    # The "xi" and "yi" entries should be "slice(None)",
+    # Others (for >2 spatial dimensions) should be an integer.
+    datslices = (index, 0, slice(None), slice(None))
+
+    if first:
+      if k == 0:
+          # Create axes
+          i, j = divmod(k, ncols)
+          mfig.paxes = mfig.add_axes(i, j, [0., 0., 1., 0.93])
+          caxes = mfig.add_axes(i, j, [0., 0.95, 1., 0.05])
+
+    # Get vertices
+    y = dset.dims[yi][0][datslices[yi]]
+    x = dset.dims[xi][0][datslices[xi]]
+    xorder = np.argsort(x)
+    yorder = np.argsort(y)
+    th = x[xorder]
+    th_mid = (th[1:] + th[:-1])/2
+    th = np.concatenate(([0], th_mid, [np.pi]))
+    r = y[yorder]
+    r = np.concatenate(([0], r))
+    yv = r[:,None]*np.cos(th[None,:])
+    if k == 0:
+        xv = r[:,None]*np.sin(th[None,:])
+    elif k == 1:
+        xv = -r[:,None]*np.sin(th[None,:])
+#    xv, yv = get_vertex_arrays(x, y)
+
+    # Colormap
+    cmap = matplotlib.cm.get_cmap(cmap_name)
+    cmap.set_bad('0.7')
+
+    # Slice data
+    data = dset[datslices]
+
+    # Transpose if necessary
+    if xi < yi:
+        data = data.T
+
+    # Sort data
+    datsort = data[yorder]
+    datsort = datsort[:, xorder]
+
+    if first:
+      # Plot
+      plot_list.append(mfig.paxes.pcolormesh(xv, yv, datsort, cmap=cmap, zorder=1))
+      if k==0:
+          mfig.paxes.axis(build_limits(mfig.paxes, xv, yv, ypad=0.0, square=False))
+          mfig.paxes.tick_params(length=0, width=0)
+
+          # Colorbar
+          cbar = mfig.figure.colorbar(plot_list[-1], cax=caxes, orientation='horizontal',
+              ticks=ticker.MaxNLocator(nbins=5))
+          cbar.outline.set_visible(False)
+          cbar.ax.xaxis.set_ticks_position('top')
+
+          # Labels
+          caxes.set_xlabel('T')
+          caxes.xaxis.set_label_position('top')
+          mfig.paxes.set_ylabel('z')
+          mfig.paxes.set_xlabel('x')
+
+    else:
+      #plot_list[k].set_array(np.ravel(datsort[:-1,:-1]))
+      plot_list[k].set_array(np.ravel(datsort))
+
+    # consistent color table across both slices
+    if k==0:
+        mfig.data_min = datsort.min()
+        mfig.data_max = datsort.max()
+    elif k==1:
+        mfig.data_min = min(mfig.data_min, datsort.min())
+        mfig.data_max = max(mfig.data_max, datsort.max())
+        plot_list[0].set_clim(mfig.data_min, mfig.data_max)
+        plot_list[1].set_clim(mfig.data_min, mfig.data_max)
+
+def build_limits(axis, x, y, xpad=0., ypad=0., square=False):
+
+    xmin, xmax, ymin, ymax = -x.max(), x.max(), y.min(), y.max()
+    dx = xmax - xmin
+    dy = ymax - ymin
+    x0 = xmin - xpad*dx
+    x1 = xmax + xpad*dx
+    y0 = ymin - ypad*dy
+    y1 = ymax + ypad*dy
+    if square:
+        ax_position = axis.get_position()
+        ax_aspect = (ax_position.height*axis.figure.get_figheight() /
+                     (ax_position.width *axis.figure.get_figwidth()))
+        if (y1 - y0) / (x1 - x0) > ax_aspect:
+            extra_w = (y1 - y0) / ax_aspect - (x1 - x0)
+            x0 -= extra_w / 2
+            x1 += extra_w / 2
+        else:
+            extra_h = ax_aspect * (x1 - x0) - (y1 - y0)
+            y0 -= extra_h / 2
+            y1 += extra_h / 2
+    return [x0, x1, y0, y1]
+
+def clean_path(path):
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+
+
+def get_vertex_arrays(x, y, trim_edges=False):
+
+    x_vvec = get_vertex_vector(x, 1, trim_edges=trim_edges)
+    y_vvec = get_vertex_vector(y, 0, trim_edges=trim_edges)
+    x_varr = x_vvec * np.ones_like(y_vvec)
+    y_varr = np.ones_like(x_vvec) * y_vvec
+
+    return x_varr, y_varr
+
+
+def get_vertex_vector(r, axis, trim_edges=False):
+
+    #axis = vector_axis(r)
+    vflat = get_vertex_flat(r.ravel(), trim_edges=trim_edges)
+
+    return reshape_vector(vflat, dim=2, axis=axis)
+
+
+def vector_axis(r):
+
+    extended = [i for (i,s) in enumerate(r.shape) if (s>1)]
+    if len(extended) == 0:
+        raise ValueError("No extended direction")
+    elif len(extended) > 1:
+        raise ValueError("Multiple extended directions")
+
+    return extended[0]
+
+
+def get_vertex_flat(grid, trim_edges=False):
+
+    diff = np.diff(grid)
+    vflat = np.zeros(grid.size+1)
+    vflat[1:-1] = grid[0:-1] + diff/2
+    if trim_edges:
+        vflat[0] = grid[0]
+        vflat[-1] = grid[-1]
+    else:
+        vflat[0] = grid[0] - diff[0]/2
+        vflat[-1] = grid[-1] + diff[-1]/2
+
+    return vflat
+
+
+def visit(filename):
+    file = h5py.File(filename, mode='r')
+
+    # Layout
+    image = Vector(1., 1.)
+    pad = Frame(0.2, 0.1, 0.1, 0.1)
+    margin = Frame(0.2, 0.1, 0.1, 0.1)
+    scale = 3.
+
+
+    # Create multifigure
+    mfig = MultiFigure(nrows, ncols, image, pad, margin, scale)
+
+    first = True
+    plot_list = []
+    timestring = ''
+    try:
+        for i in range(start,len(file['scales']['sim_time'])):
+            timestring = main(file, i, first, plot_list, timestring, mfig)
+            first = False
+    finally:
+        file.close()
+
+    plt.close(mfig.figure)
+
+
+class Vector:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @property
+    def vx(self):
+        return Vector(self.x, 0)
+
+    @property
+    def vy(self):
+        return Vector(0, self.y)
+
+    def __add__(self, other):
+        if np.isscalar(other):
+            return Vector(self.x + other, self.y + other)
+        elif isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y)
+        return NotImplemented
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        if np.isscalar(other):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return Vector(self.x * other.x, self.y * other.y)
+        return NotImplemented
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        if np.isscalar(other):
+            return Vector(self.x / other, self.y / other)
+        elif isinstance(other, Vector):
+            return Vector(self.x / other.x, self.y / other.y)
+        return NotImplemented
+
+
+class Frame:
+
+    def __init__(self, top, bottom, left, right):
+        self.bottom_left = Vector(left, bottom)
+        self.top_right = Vector(right, top)
+
+    @staticmethod
+    def from_vectors(bottom_left, top_right):
+        return Frame(top_right.y, bottom_left.y, bottom_left.x, top_right.x)
+
+    @property
+    def bottom(self):
+        return self.bottom_left.y
+
+    @property
+    def left(self):
+        return self.bottom_left.x
+
+    @property
+    def top(self):
+        return self.top_right.y
+
+    @property
+    def right(self):
+        return self.top_right.x
+
+    def __add__(self, other):
+        bottom_left = self.bottom_left + other
+        top_right = self.top_right + other
+        return Frame.from_vectors(bottom_left, top_right)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __mul__(self, other):
+        bottom_left = self.bottom_left * other
+        top_right = self.top_right * other
+        return Frame.from_vectors(bottom_left, top_right)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __truediv__(self, other):
+        bottom_left = self.bottom_left / other
+        top_right = self.top_right / other
+        return Frame.from_vectors(bottom_left, top_right)
+
+
+class MultiFigure:
+
+    def __init__(self, nrows, ncols, image, pad, margin, scale=1., **kw):
+
+        # Build composite vectors
+        sub = pad.bottom_left + image + pad.top_right
+        subs = nrows*sub.vy + ncols*sub.vx
+        fig = margin.bottom_left + subs + margin.top_right
+
+        # Rectify scaling so fig dimensions are integers
+        newscale = np.ceil(scale*fig.y) / fig.y
+        x_extra = np.ceil(newscale*fig.x) - newscale*fig.x
+        extra = Vector(x_extra, 0)
+
+        # Apply scale
+        image *= newscale
+        pad *= newscale
+        margin *= newscale
+        margin += extra / 2
+
+        # Build composite vectors
+        sub = pad.bottom_left + image + pad.top_right
+        subs = nrows*sub.vy + ncols*sub.vx
+        fig = margin.bottom_left + subs + margin.top_right
+
+        # Build figure
+        figx = int(np.rint(fig.x))
+        figy = int(np.rint(fig.y))
+        self.figure = plt.figure(figsize=(figx, figy), **kw)
+
+        # Attributes
+        self.nrows = nrows
+        self.ncols = ncols
+        self.image = image
+        self.pad = pad
+        self.margin = margin
+        self.sub = sub
+        self.subs = subs
+        self.fig = fig
+
+    def add_axes(self, i, j, rect, **kw):
+
+        # References
+        margin = self.margin
+        nrows = self.nrows
+        sub = self.sub
+        pad = self.pad
+        image = self.image
+        fig = self.fig
+
+        start = margin.bottom_left + (nrows-1-i)*sub.vy + j*sub.vx + pad.bottom_left
+        left, bottom, width, height = rect
+        axstart = (start + Vector(left, bottom)*image) / fig
+        axshape = (Vector(width, height)*image) / fig
+        normrect = [axstart.x, axstart.y, axshape.x, axshape.y]
+        ax = self.figure.add_axes(normrect, **kw)
+
+        return ax
+
+if __name__ == "__main__":
+
+    if MPI.COMM_WORLD.rank == 0:
+        if not os.path.exists('%s/' %saveroot):
+            os.mkdir('%s/' %saveroot)
+    MPI.COMM_WORLD.barrier()
+
+    rank = MPI.COMM_WORLD.rank
+    size = MPI.COMM_WORLD.size
+
+    dir = sys.argv[1]
+
+    if len(sys.argv)>2:
+      num_start = sys.argv[2]
+    else:
+      num_start = 1
+
+    if len(sys.argv)>3:
+      num_end = sys.argv[3]
+    else:
+      # find number of files
+      base_path = pathlib.Path(sys.argv[1]).absolute()
+      folder_paths = base_path.glob('%s_f*' %base_path.stem)
+      folder_paths = general.natural_sort(folder_paths)
+      num_end = len(folder_paths) + (num_start-1)
+
+
+    for i in range(int(num_start)+rank,int(num_end)+1,size):
+      filename = dir+"/"+dir+"_s"+str(i)+".h5"
+      print(filename)
+      visit(filename)
+#      proc = multiprocessing.Process(target=visit, args=(filename,main))
+#      proc.daemon=True
+#      proc.start()
+#      proc.join()
+
+
