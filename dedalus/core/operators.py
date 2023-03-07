902ec22e99212aeec08d64ee68e2bb425ad9894d
Calum Skene <c.s.skene@leeds.ac.uk>
2023-03-07 13:39:26 +0000

    Alter adjoint operators to take an input to enable the original direct operator to dictate the input basis. Update adjoint tests to use adjoint bases.

--- dedalus/core/operators.py
+++ dedalus/core/operators.py
@@ -1008,9 +1008,9 @@ class SpectralOperator1D(SpectralOperator):
         else:
             out.data.fill(0)
 
-    def operate_adjoint(self, out):
+    def operate_adjoint(self, input, out):
         """Perform adjoint operation."""
-        arg = self.args[0]
+        arg = input
         layout = arg.layout
         # Set output layout
         out.preset_layout(layout)
@@ -1668,6 +1668,18 @@ class Convert(SpectralOperator, metaclass=MultiClass):
         # Revert to matrix application for coeff space
         else:
             super().operate(out)
+
+    def operate_adjoint(self, input, out):
+        """Perform operation."""
+        arg = input
+        layout = arg.layout
+        # Copy for grid space
+        if layout.grid_space[self.last_axis]:
+            out.preset_layout(layout)
+            np.copyto(out.data, arg.data)
+        # Revert to matrix application for coeff space
+        else:
+            super().operate_adjoint(arg,out)
 
 
 class ConvertSame(Convert):
--- dedalus/tests/test_adjoint_operators.py
+++ dedalus/tests/test_adjoint_operators.py
@@ -1,4 +1,4 @@
-"""Test Jacobi conversion, differentiation, interpolation, and integration."""
+"""Test cartesian adjoint differentiation and Jacobi conversion."""
 
 import pytest
 import numpy as np
@@ -8,39 +8,39 @@ from dedalus.core import coords, distributor, basis, field, operators, problems,
 
 N_range = [8, 10, 12]
 bounds_range = [(0, 2*np.pi), (0.5, 1.5)]
-dtype_range = [np.float64, np.complex128]
+dtype_range = [np.float64]
 
 @pytest.mark.parametrize('N', N_range)
 @pytest.mark.parametrize('bounds', bounds_range)
 @pytest.mark.parametrize('dtype', dtype_range)
-def test_fourier_differentiate(N, bounds, dtype):
+@pytest.mark.parametrize('layout', ['g','c'])
+def test_fourier_adjoint_differentiate(N, bounds, dtype, layout):
     c = coords.Coordinate('x')
     d = distributor.Distributor((c,))
     if dtype == np.float64:
-        b = basis.RealFourier(c, size=N, bounds=bounds)
+        b_dir = basis.RealFourier(c, size=N, bounds=bounds)
+        b_adj = basis.RealFourier(c, size=N, bounds=bounds, adjoint=True)
     elif dtype == np.complex128:
-        b = basis.ComplexFourier(c, size=N, bounds=bounds)
-    x = b.local_grid(1)
+        b_dir = basis.ComplexFourier(c, size=N, bounds=bounds)
+        b_adj = basis.ComplexFourier(c, size=N, bounds=bounds, adjoint=True) 
 
-    u    = field.Field(dist=d, bases=b,dtype=dtype)
-    Cu   = field.Field(dist=d, bases=b,dtype=dtype)
-    v    = field.Field(dist=d, bases=b,dtype=dtype) 
-    CHv  = field.Field(dist=d, bases=b,dtype=dtype) 
-    x = b.local_grid(1)
+    u    = field.Field(dist=d, bases=(b_dir,),dtype=dtype)
+    Cu   = field.Field(dist=d, bases=(b_dir,),dtype=dtype)
+    v    = field.Field(dist=d, bases=(b_adj,),dtype=dtype) 
+    CHv  = field.Field(dist=d, bases=(b_adj,),dtype=dtype) 
 
-    u.fill_random()
-    v.fill_random()
-    # Must be in coefficient space for now
-    u['c']
-    v['c']
-    operators.Differentiate(u, c).operate(Cu)
-    operators.Differentiate(v, c).operate_adjoint(CHv)
+    u.fill_random(layout=layout)
+    v.fill_random(layout=layout)
+    # Real doesn't work in grid space...
+    u[layout]
+    v[layout]
+    differentiate = operators.Differentiate(u, c) 
+    differentiate.operate(Cu)
+    differentiate.operate_adjoint(v,CHv)
     term1 = np.vdot(v['c'],Cu['c'])
     term2 = np.vdot(CHv['c'],u['c'])
 
     assert np.allclose(term1,term2)
-
-
 
 # Jacobi tests
 N_range = [8, 9]
@@ -55,25 +55,35 @@ d_range = [1,2,3]
 @pytest.mark.parametrize('k', k_range)
 @pytest.mark.parametrize('dtype', dtype_range)
 @pytest.mark.parametrize('d_range', d_range)
-def test_jacobi_convert_adjoint(N, a, b, k, dtype, d_range):
+@pytest.mark.parametrize('layout', ['g','c'])
+def test_jacobi_convert_adjoint(N, a, b, k, dtype, d_range, layout):
     c = coords.Coordinate('x')
     d = distributor.Distributor((c,))
-    b = basis.Jacobi(c, size=N, a0=a, b0=b, a=a+k, b=b+k, bounds=(0, 1))
-    b1 = b.derivative_basis(d_range)
+    b_dir = basis.Jacobi(c, size=N, a0=a, b0=b, a=a+k, b=b+k, bounds=(0, 1))
+    b_adj = basis.Jacobi(c, size=N, a0=a, b0=b, a=a+k, b=b+k, bounds=(0, 1), adjoint=True)
+    b1     = b_dir.derivative_basis(d_range)
+    b1_adj = b_adj.derivative_basis(d_range)
 
-    u    = field.Field(dist=d, bases=b,dtype=dtype)
-    Cu   = field.Field(dist=d, bases=b,dtype=dtype)
-    v    = field.Field(dist=d, bases=b,dtype=dtype) 
-    CHv  = field.Field(dist=d, bases=b,dtype=dtype) 
-    x = b.local_grid(1)
+       
+    # For adjoint v should be in the adjoint basis of Cu, 
+    # and CHv will be in the adjoint basis of u.
+    u    = field.Field(dist=d, bases=b_dir,dtype=dtype)
+    Cu   = field.Field(dist=d, bases=b1,dtype=dtype)
+    v  = field.Field(dist=d, bases=b1_adj,dtype=dtype)
+    CHv  = field.Field(dist=d, bases=b_adj,dtype=dtype) 
 
-    u.fill_random()
-    v.fill_random()
-    # Must be in coefficient space for now
-    u['c']
-    v['c']
-    operators.convert(u, (b1,)).operate(Cu)
-    operators.convert(v, (b1,)).operate_adjoint(CHv)
+    u.fill_random(layout=layout)
+    v.fill_random(layout=layout)
+
+    u[layout]
+    v[layout]
+    
+    # Convert is based on the basis of first argument...
+    # So need a new input for operate adjoint, but must use original
+    # convert operator
+    convert = operators.convert(u, (b1,))
+    convert.operate(Cu)
+    convert.operate_adjoint(v,CHv)
     term1 = np.vdot(v['c'],Cu['c'])
     term2 = np.vdot(CHv['c'],u['c'])
 
@@ -84,25 +94,29 @@ def test_jacobi_convert_adjoint(N, a, b, k, dtype, d_range):
 @pytest.mark.parametrize('b', ab_range)
 @pytest.mark.parametrize('k', k_range)
 @pytest.mark.parametrize('dtype', dtype_range)
-def test_jacobi_differentiate_adjoint(N, a, b, k, dtype):
+@pytest.mark.parametrize('layout', ['g','c'])
+def test_jacobi_differentiate_adjoint(N, a, b, k, dtype, layout):
     c = coords.Coordinate('x')
     d = distributor.Distributor((c,))
-    b = basis.Jacobi(c, size=N, a0=a, b0=b, a=a+k, b=b+k, bounds=(0, 1))
-    x = b.local_grid(1)
-    
-    u    = field.Field(dist=d, bases=b,dtype=dtype)
-    Cu   = field.Field(dist=d, bases=b,dtype=dtype)
-    v    = field.Field(dist=d, bases=b,dtype=dtype) 
-    CHv  = field.Field(dist=d, bases=b,dtype=dtype) 
-    x = b.local_grid(1)
+    b_dir = basis.Jacobi(c, size=N, a0=a, b0=b, a=a+k, b=b+k, bounds=(0, 1))
+    b_adj = basis.Jacobi(c, size=N, a0=a, b0=b, a=a+k, b=b+k, bounds=(0, 1),adjoint=True)
 
-    u.fill_random()
-    v.fill_random()
-    # Must be in coefficient space for now
-    u['c']
-    v['c']
-    operators.Differentiate(u, c).operate(Cu)
-    operators.Differentiate(v, c).operate_adjoint(CHv)
+    b1     = b_dir.derivative_basis(1)
+    b1_adj = b_adj.derivative_basis(1)
+    
+    u    = field.Field(dist=d, bases=(b_dir,),dtype=dtype)
+    Cu   = field.Field(dist=d, bases=(b1,),dtype=dtype)
+    v    = field.Field(dist=d, bases=(b1_adj,),dtype=dtype)
+    CHv  = field.Field(dist=d, bases=(b_adj,),dtype=dtype) 
+
+    u.fill_random(layout=layout)
+    v.fill_random(layout=layout)
+
+    u[layout]
+    v[layout]
+    differentiate = operators.Differentiate(u, c) 
+    differentiate.operate(Cu)
+    differentiate.operate_adjoint(v,CHv)
     term1 = np.vdot(v['c'],Cu['c'])
     term2 = np.vdot(CHv['c'],u['c'])
 
